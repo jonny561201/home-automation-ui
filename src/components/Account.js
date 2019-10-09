2eca4dbb4b451807f7c1466004ca06cf60909ba2
Jon Graf <jongraf1@gmail.com>
2019-10-09 18:59:29 -0500

    added account status button for header

---
+++ src/components/Account.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Account.css';
+
+function Account() {
+    return (
+        <div className="account-container">
+            <div className="account-border">
+                <div className="account-center"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Account;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import LogoHeader from './LogoHeader';
+import Account from './Account';
 
 function Home() {
     return (
@@ -8,6 +9,7 @@ function Home() {
             <div className="home-header">
                 <LogoHeader />
                 <h1 className="home-header-text">Home Automation</h1>
+                <Account />
             </div>
             <div className="home-body">body</div>
         </div>
