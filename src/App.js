9fde0e15b43b59d88c21285e4494b40729fafabf
Jon Graf <jongraf1@gmail.com>
2019-10-12 13:28:45 -0500

    WIP: attempting to get navigation working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
-export default function App() {
+
+
+const MainMenu = () => {
   return (
-    <header className="App-header">
-      <Login />
-    </header>
-  );
+    <div>
+      <Link to="/">
+        <button>Login</button>
+      </Link>
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
+    </div>
+  )
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <header className="App-header">
+          {/* <Home /> */}
+          <MainMenu />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+        </header>
+      </Router>
+    );
+  }
 }
