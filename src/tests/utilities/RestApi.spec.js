fd613b2781cf4f4aa03e19e1317e3e03cef388dd
Jon Graf <jongraf1@gmail.com>
2019-10-19 11:48:26 -0500

    added function to query garage door status

--- src/tests/utilities/RestApi.spec.js
+++ src/tests/utilities/RestApi.spec.js
@@ -1,15 +1,16 @@
 import base64 from 'base-64';
 import fetchMock from 'fetch-mock';
-import { getBearerToken } from '../../utilities/RestApi';
+import { getBearerToken, getGarageStatus } from '../../utilities/RestApi';
 
 describe('RestApi', () => {
     const username = 'fakeUser';
     const password = 'fakepass';
     const fakeBearerToken = "fakeBearerToken";
-    const options = {"method": "GET", "headers": {'Authorization': 'Basic ' + base64.encode(username + ":" + password)}};
-    const response = { 'bearerToken': fakeBearerToken };
-
+    
     it('should make rest call to login api using auth header', async () => {
+        const response = { 'bearerToken': fakeBearerToken };
+        const options = {"method": "GET", "headers": {'Authorization': 'Basic ' + base64.encode(username + ":" + password)}};
+
         fetchMock.mock('http://localhost:5000/login', response, options).catch(unmatchedUrl => {
             return {
                 status: 400,
@@ -18,7 +19,18 @@ describe('RestApi', () => {
         });
 
         const actual = await getBearerToken(username, password);
-        console.log('Response: ' + JSON.stringify(response));
         expect(actual.bearerToken).toEqual(fakeBearerToken);
+    });
+
+    it('should make rest call to get garage door state', async () => {
+        const response = {'isGarageOpen': true}
+        const options = {"method": "GET", "headers": {'Authorization': 'Bearer ' + fakeBearerToken}};
+
+        fetchMock.mock('http://localhost:5000/garageDoor/status', response, options).catch(unmatchedUrl => {
+            return { status: 400}
+        });
+
+        const actual = await getGarageStatus();
+        expect(actual.isGarageOpen).toEqual(true);
     });
 });
--- src/utilities/RestApi.js
+++ src/utilities/RestApi.js
@@ -9,3 +9,13 @@ export async function getBearerToken(username, password) {
     const response = await fetch('http://localhost:5000/login', options);
     return await response.json();
 }
+
+export async function getGarageStatus() {
+    const options = {
+        method: 'GET',
+        headers: {'Authorization': 'Bearer ' + 'fakeBearerToken'}
+    };
+
+    const response = await fetch('http://localhost:5000/garageDoor/status', options);
+    return await response.json();
+}
