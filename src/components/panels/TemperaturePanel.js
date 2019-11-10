bc2713afdefe551c8285e17543300384ba9a7f19
Jon Graf <jgraf@leantechniques.com>
2019-11-10 15:48:01 -0600

    updated temperature panel to make api call to get weather and temp data

--- src/components/panels/TemperaturePanel.js
+++ src/components/panels/TemperaturePanel.js
@@ -5,23 +5,33 @@ import { ExpansionPanelDetails, ExpansionPanel, Typography, ExpansionPanelSummar
 import './TemperaturePanel.css';
 
 
-export default function TemperaturePanel() {
-    return (
-        <div>
-            <ExpansionPanel className="temperature-panel">
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <div className="summary">
-                        <div>
-                            <img alt="temperature" className="logo-image" src={TemperatureIcon} />
+export default class TemperaturePanel extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.apiRequests.getCurrentTemperature(this.props.apiRequests.userId);
+    }
+
+    render() {
+        return (
+            <div>
+                <ExpansionPanel className="temperature-panel">
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <div className="summary">
+                            <div>
+                                <img alt="temperature" className="logo-image" src={TemperatureIcon} />
+                            </div>
+                            <Typography className="panel-text">Temperature</Typography>
                         </div>
-                        <Typography className="panel-text">Temperature</Typography>
-                    </div>
-                </ExpansionPanelSummary>
-                <Divider />
-                <ExpansionPanelDetails className="center">
-                    <Typography>Test Detail line 2</Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-        </div>
-    );
+                    </ExpansionPanelSummary>
+                    <Divider />
+                    <ExpansionPanelDetails className="center">
+                        <Typography>Test Detail line 2</Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+        );
+    }
 }
--- src/tests/components/panels/TemperaturePanel.spec.js
+++ src/tests/components/panels/TemperaturePanel.spec.js
@@ -4,9 +4,13 @@ import TemperaturePanel from '../../../components/panels/TemperaturePanel';
 
 describe('TemperaturePanel', () => {
     let tempPanel;
+    let userId = 'fakeUserId'
+    let mockRequests = jest.fn();
+    let apiRequests = { getCurrentTemperature: mockRequests, userId: userId };
 
     beforeEach(() => {
-        tempPanel = shallow(<TemperaturePanel />);
+        mockRequests.mockClear();
+        tempPanel = shallow(<TemperaturePanel apiRequests={apiRequests} />);
     });
 
     it('should display temperature panel', () => {
@@ -17,5 +21,12 @@ describe('TemperaturePanel', () => {
     it('should display temperature icon', () => {
         const actual = tempPanel.find('.temperature-panel img').prop('alt');
         expect(actual).toEqual('temperature');
+    });
+
+    describe('ComponentDidMount', () => {
+        it('should make api to get current weather', () => {
+            expect(mockRequests).toHaveBeenCalledTimes(1);
+            expect(mockRequests).toBeCalledWith(userId);
+        });
     });
 });
