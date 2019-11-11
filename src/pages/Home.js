3c6f29028660c462a31ff71b0d6a60a67e5b219a
Jon Graf <jgraf@leantechniques.com>
2019-11-10 18:12:44 -0600

    cut over home page to use the new Header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react';
-import LogoHeader from '../components/header/LogoHeader';
-import Account from '../components/header/Account';
-import AccountSettings from '../components/header/AccountSettings';
+import Header from '../components/header/Header';
+// import LogoHeader from '../components/header/LogoHeader';
+// import Account from '../components/header/Account';
+// import AccountSettings from '../components/header/AccountSettings';
 import DashboardPanel from '../components/panels/DashboardPanels';
 import './Home.css';
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
-        this.toggleAccountSettings = this.toggleAccountSettings.bind(this);
-        this.state = {
-            settingsActive: false
-        }
-    }
-
-    toggleAccountSettings = () => {
-        this.setState({ settingsActive: !this.state.settingsActive })
     }
 
     render() {
         return (
             <div className="home-main" >
-                <div className="home-header">
-                    <div className="test">
-                        <LogoHeader />
-                    </div>
-                    <div></div>
-                    <div>
-                        <h1 className="home-header-text">Home Automation</h1>
-                    </div>
-                    <Account toggle={this.toggleAccountSettings} />
-                </div>
-                {this.state.settingsActive
-                    ? <AccountSettings />
-                    : <div></div>
-                }
+                <Header />
                 <div className="home-body">
                     <div className="center">
                         <DashboardPanel apiRequests={this.props.apiRequests} />
--- src/tests/pages/Home.spec.js
+++ src/tests/pages/Home.spec.js
@@ -10,63 +10,18 @@ describe('Home', () => {
         home = shallow(<Home />);
     });
 
-    it('should display header bar', () => {
-        const header = home.find('.home-header');
-        expect(header).toHaveLength(1)
-    });
-
-    it('should display header text', () => {
-        const header = home.find('h1').text();
-        expect(header).toEqual('Home Automation');
-    });
-
-    it('should display company logo', () => {
-        const header = home.find('LogoHeader');
-        expect(header).toHaveLength(1);
-    });
-
-    it('should display account icon', () => {
-        const header = home.find('Account');
-        expect(header).toHaveLength(1);
+    it('should display Header component', () => {
+        const actual = home.find('Header');
+        expect(actual).toHaveLength(1);
     });
 
     it('should display page body', () => {
-        const body = home.find('.home-body');
-        expect(body).toHaveLength(1)
+        const actual = home.find('.home-body');
+        expect(actual).toHaveLength(1)
     });
 
-    describe('AccountSettings', () => {
-        it('should not show account settings when settingsActive false', () => {
-            home.state().settingsActive = false;
-            const accountSettings = home.find('AccountSettings');
-            expect(accountSettings).toHaveLength(0);
-        });
-
-        it('should show account settings when settingsActive true', () => {
-            home.state().settingsActive = true;
-            home.instance().forceUpdate();
-            const accountSettings = home.find('AccountSettings');
-            expect(accountSettings).toHaveLength(1);
-        });
-    });
-
-    describe('toggleAccountSettings', () => {
-        let instance;
-
-        beforeEach(() => {
-            instance = home.instance();
-        });
-
-        it('should set state to true when false', () => {
-            home.state().settingsActive = false;
-            instance.toggleAccountSettings();
-            expect(home.state().settingsActive).toBeTruthy;
-        });
-
-        it('should set state to false when true', () => {
-            home.state().settingsActive = true;
-            instance.toggleAccountSettings();
-            expect(home.state().settingsActive).toBeFalsy;
-        });
+    it('should display DashboardPanel', () => {
+        const actual = home.find('DashboardPanel');
+        expect(actual).toHaveLength(1);
     });
 });
