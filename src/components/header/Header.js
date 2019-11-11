24f2a9953ec0fb4242d91d14a3591e5bf913ae06
Jon Graf <jgraf@leantechniques.com>
2019-11-10 18:12:18 -0600

    extracted header to reusable component

---
+++ src/components/header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import LogoHeader from '../header/LogoHeader';
+import Account from '../header/Account';
+import AccountSettings from '../header/AccountSettings';
+
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            settingsActive: false,
+        }
+    }
+
+    toggleAccountSettings = () => {
+        this.setState({ settingsActive: !this.state.settingsActive })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="home-header">
+                    <div className="test">
+                        <LogoHeader />
+                    </div>
+                    <div></div>
+                    <div>
+                        <h1 className="home-header-text">Home Automation</h1>
+                    </div>
+                    <Account toggle={this.toggleAccountSettings} />
+                </div>
+                {this.state.settingsActive
+                    ? <AccountSettings />
+                    : <div></div>
+                }
+            </div>
+        );
+    }
+}
---
+++ src/tests/components/header/Header.spec.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../../components/header/Header';
+
+describe('HeaderComponent', () => {
+    let header;
+
+    beforeEach(() => {
+        header = shallow(<Header />)
+    });
+
+    it('should display header bar', () => {
+        const actual = header.find('.home-header');
+        expect(actual).toHaveLength(1)
+    });
+
+    it('should display header text', () => {
+        const actual = header.find('h1').text();
+        expect(actual).toEqual('Home Automation');
+    });
+
+    it('should display company logo', () => {
+        const actual = header.find('LogoHeader');
+        expect(actual).toHaveLength(1);
+    });
+
+    it('should display account icon', () => {
+        const actual = header.find('Account');
+        expect(actual).toHaveLength(1);
+    });
+
+    describe('AccountSettings', () => {
+        it('should not show account settings when settingsActive false', () => {
+            header.state().settingsActive = false;
+            const accountSettings = header.find('AccountSettings');
+            expect(accountSettings).toHaveLength(0);
+        });
+
+        it('should show account settings when settingsActive true', () => {
+            header.state().settingsActive = true;
+            header.instance().forceUpdate();
+            const accountSettings = header.find('AccountSettings');
+            expect(accountSettings).toHaveLength(1);
+        });
+    });
+
+    describe('toggleAccountSettings', () => {
+        let instance;
+
+        beforeEach(() => {
+            instance = header.instance();
+        });
+
+        it('should set state to true when false', () => {
+            header.state().settingsActive = false;
+            instance.toggleAccountSettings();
+            expect(header.state().settingsActive).toBeTruthy;
+        });
+
+        it('should set state to false when true', () => {
+            header.state().settingsActive = true;
+            instance.toggleAccountSettings();
+            expect(header.state().settingsActive).toBeFalsy;
+        });
+    });
+});
