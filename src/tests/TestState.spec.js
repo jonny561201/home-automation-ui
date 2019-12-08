e31a05f7676aeca9ff3c94d7b65dc6502434bd1c
Jon Graf <jongraf1@gmail.com>
2019-12-08 16:59:30 -0600

    backfilled test coverage for state to set authorization

--- src/tests/TestState.spec.js
+++ src/tests/TestState.spec.js
@@ -10,10 +10,33 @@ describe('TestState', () => {
             state = useState()
         });
 
-        it('should setActivePage', () => {
+        it('should set active page', () => {
             state.setActivePage(activePage);
             const actual = state.state.activePage;
             expect(actual).toEqual(activePage);
+        });
+
+        it('should get active page', () => {
+            state.state.activePage = activePage;
+            expect(state.getActivePage()).toEqual(activePage);
+        });
+    });
+
+    describe('Authorization', () => {
+        let state;
+
+        beforeEach(() => {
+            state = useState();
+        });
+
+        it('should update the authorization to true', () => {
+            state.updateAuth(true);
+            expect(state.state.isAuthenticated).toBeTruthy();
+        });
+
+        it('should update the authorization to false', () => {
+            state.updateAuth(false);
+            expect(state.state.isAuthenticated).toBeFalsy();
         });
     });
 });
