289ae077785d89b60a9ff0a85da293f70f0a0fbd
Jon Graf <jgraf@leantechniques.com>
2019-12-09 23:28:12 -0600

    backfilled remaining test coverage for account settings

--- src/tests/components/header/AccountSettings.spec.js
+++ src/tests/components/header/AccountSettings.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { getStore } from '../../../TestState';
 import AccountSettings from '../../../components/header/AccountSettings';
 
 
@@ -7,11 +8,27 @@ describe('AccountSettings', () => {
     let accountSettings;
 
     beforeEach(() => {
-        accountSettings = shallow(<AccountSettings />)
+        accountSettings = shallow(<AccountSettings />);
     });
 
     it('should display sign out link', () => {
         const actual = accountSettings.find('.account-button').at(1).text();
         expect(actual).toEqual('Sign Out');
+    });
+
+    it('should display Settings link when active page is set to home', () => {
+        getStore().setActivePage('Home');
+        accountSettings.setProps({});
+
+        const actual = accountSettings.find('.account-button').at(0).text();
+        expect(actual).toEqual('Settings');
+    });
+
+    it('should display Home link when active page is set to settings', () => {
+        getStore().setActivePage('Settings');
+        accountSettings.setProps({});
+
+        const actual = accountSettings.find('.account-button').at(0).text();
+        expect(actual).toEqual('Home');
     });
 });
