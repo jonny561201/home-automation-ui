5cf6f3204d92bf83df8373d443240c98987405e0
Jon Graf <jgraf@leantechniques.com>
2019-12-27 22:54:54 -0600

    added depth unit of measure to display in the sump pump basement panel

--- src/components/panels/BasementPanel.js
+++ src/components/panels/BasementPanel.js
@@ -4,8 +4,8 @@ import BasementIcon from '../../resources/BasementIcon.jpg';
 import SumpPumpIcon from '../../resources/SumpPumpIcon.png';
 import { ExpansionPanelDetails, ExpansionPanel, Typography, ExpansionPanelSummary, Divider } from '@material-ui/core';
 import './BasementPanel.css';
-import {getSumpLevels} from '../../utilities/RestApi';
-import {getStore} from '../../TestState';
+import { getSumpLevels } from '../../utilities/RestApi';
+import { getStore } from '../../TestState';
 
 
 export default class BasementPanel extends React.Component {
@@ -14,15 +14,19 @@ export default class BasementPanel extends React.Component {
         this.state = {
             currentSumpDepth: 0.0,
             averageSumpDepth: 0.0,
+            depthUnit: null,
             warningLevel: 0,
         }
     }
 
     componentDidMount = async () => {
         const response = await getSumpLevels(getStore().getUserId());
-        this.setState({ warningLevel: response.warningLevel });
-        this.setState({ currentSumpDepth: response.currentDepth });
-        this.setState({ averageSumpDepth: response.averageDepth });
+        this.setState({
+            warningLevel: response.warningLevel,
+            depthUnit: response.depthUnit,
+            currentSumpDepth: parseFloat(response.currentDepth.toFixed(1)),
+            averageSumpDepth: parseFloat(response.averageDepth.toFixed(1))
+        });
     }
 
     render() {
@@ -45,10 +49,12 @@ export default class BasementPanel extends React.Component {
                                 <div className="sump-text-group">
                                     <p className="current-text sump-text">Current: </p>
                                     <p className={"current-depth sump-text " + (this.state.warningLevel === 3 ? 'alert' : 'healthy')}>{this.state.currentSumpDepth}</p>
+                                    <p className="current-text sump-text">{this.state.depthUnit}</p>
                                 </div>
                                 <div className="sump-text-group">
                                     <p className="average-text sump-text">Average: </p>
                                     <p className="average-depth sump-text">{this.state.averageSumpDepth}</p>
+                                    <p className="average-text sump-text">{this.state.depthUnit}</p>
                                 </div>
                             </div>
                         </div>
