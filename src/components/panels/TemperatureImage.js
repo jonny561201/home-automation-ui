4c21ee6e2f5657eec9848b8b1fd787b81918192c
Jon Graf <jgraf@leantechniques.com>
2020-01-25 14:13:13 -0600

    refactored temperature image to clean up all the case statements

--- src/components/panels/TemperatureImage.js
+++ src/components/panels/TemperatureImage.js
@@ -1,55 +1,39 @@
 import React from 'react';
 import ClearIcon from '../../resources/weatherIcons/clear.png';
 import CloudyIcon from '../../resources/weatherIcons/cloudy.png';
-import PartlyCloudy from '../../resources/weatherIcons/partly_cloudy.png';
-import Snowy from '../../resources/weatherIcons/snowy.png';
-import LightSnow from '../../resources/weatherIcons/light_snow.png';
-import LightRain from '../../resources/weatherIcons/light_rain.png';
-import HeavyRain from '../../resources/weatherIcons/heavy_rain.png';
-import Thunderstorm from '../../resources/weatherIcons/thunderstorm.png';
+import PartlyCloudyIcon from '../../resources/weatherIcons/partly_cloudy.png';
+import SnowyIcon from '../../resources/weatherIcons/snowy.png';
+import LightSnowIcon from '../../resources/weatherIcons/light_snow.png';
+import LightRainIcon from '../../resources/weatherIcons/light_rain.png';
+import HeavyRainIcon from '../../resources/weatherIcons/heavy_rain.png';
+import ThunderstormIcon from '../../resources/weatherIcons/thunderstorm.png';
 import HomeIcon from '../../resources/weatherIcons/home.png';
 import './TemperatureImage.css'
 
 
 export default function TemperatureImage(props) {
 
+    const lightRain = ["drizzle", "drizzle rain", "light rain", "moderate rain"];
+    const heavyRain = ["heavy intensity rain", "very heavy rain", "extreme rain", "shower rain", "heavy intensity shower rain"];
+
     const getWeatherImage = () => {
-        switch (props.description.toLowerCase()) {
-            case "clear sky":
-                return <img className="weather-icon" alt="description" src={ClearIcon} label="clear sky" />;
-            case "heavy intensity rain":
-            case "very heavy rain":
-            case "extreme rain":
-            case "shower rain":
-            case "heavy intensity shower rain":
-                return <img className="weather-icon" alt="description" src={HeavyRain} label="heavy rain" />;
-            case "few clouds":
-            case "scattered clouds":
-                return <img className="weather-icon" alt="description" src={PartlyCloudy} label="partly cloudy" />
-            case "drizzle":
-            case "drizzle rain":
-            case "light rain":
-            case "moderate rain":
-                return <img className="weather-icon" alt="description" src={LightRain} label="light rain" />
-            case "snow":
-            case "light snow":
-                return <img className="weather-icon" alt="description" src={LightSnow} label="light snow" />
-            case "heavy snow":
-            case "sleet":
-                return <img className="weather-icon" alt="description" src={Snowy} label="heavy snow" />
-            case "thunderstorm with light rain":
-            case "thunderstorm with rain":
-            case "thunderstorm with heavy rain":
-            case "light thunderstorm":
-            case "heavy thunderstorm":
-            case "thunderstorm with drizzle":
-            case "thunderstorm with light drizzle":
-            case "thunderstorm with heavy drizzle":
-            case "thunderstorm":
-            case "ragged thunderstorm":
-                return <img className="weather-icon" alt="description" src={Thunderstorm} label="thunderstorms" />
-            default:
-                return <img className="weather-icon" alt="description" src={CloudyIcon} label="cloudy" />;
+        const weatherDesc = props.description.toLowerCase();
+        if (weatherDesc.includes("thunderstorm")) {
+            return <img className="weather-icon" alt="description" src={ThunderstormIcon} label="thunderstorms" />
+        } else if (weatherDesc === "clear sky") {
+            return <img className="weather-icon" alt="description" src={ClearIcon} label="clear sky" />;
+        } else if (heavyRain.includes(weatherDesc)) {
+            return <img className="weather-icon" alt="description" src={HeavyRainIcon} label="heavy rain" />;
+        } else if (lightRain.includes(weatherDesc)) {
+            return <img className="weather-icon" alt="description" src={LightRainIcon} label="light rain" />
+        } else if (weatherDesc === "few clouds" || weatherDesc === "scattered clouds") {
+            return <img className="weather-icon" alt="description" src={PartlyCloudyIcon} label="partly cloudy" />;
+        } else if (weatherDesc === "snow" || weatherDesc === "light snow") {
+            return <img className="weather-icon" alt="description" src={LightSnowIcon} label="light snow" />
+        } else if (weatherDesc === "heavy snow" || weatherDesc === "sleet") {
+            return <img className="weather-icon" alt="description" src={SnowyIcon} label="heavy snow" />
+        } else {
+            return <img className="weather-icon" alt="description" src={CloudyIcon} label="cloudy" />;
         }
     }
 
