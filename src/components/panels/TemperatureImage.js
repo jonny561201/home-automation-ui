7202dd546f401d5675f2f89657f404b1aa003af0
Jon Graf <jongraf1@gmail.com>
2020-01-25 10:58:37 -0600

    added heavy rain to the weather icons

--- src/components/panels/TemperatureImage.js
+++ src/components/panels/TemperatureImage.js
@@ -5,6 +5,7 @@ import PartlyCloudy from '../../resources/weatherIcons/partly_cloudy.png';
 import Snowy from '../../resources/weatherIcons/snowy.png';
 import LightSnow from '../../resources/weatherIcons/light_snow.png';
 import LightRain from '../../resources/weatherIcons/light_rain.png';
+import HeavyRain from '../../resources/weatherIcons/heavy_rain.png';
 import Thunderstorm from '../../resources/weatherIcons/thunderstorm.png';
 import './TemperatureImage.css'
 
@@ -15,8 +16,12 @@ export default function TemperatureImage(props) {
         switch (props.description.toLowerCase()) {
             case "clear sky":
                 return <img className="weather-icon" alt="description" src={ClearIcon} label="clear sky" />;
-            case "overcast clouds":
-                return <img className="weather-icon" alt="description" src={CloudyIcon} label="broken clouds" />;
+            case "heavy intensity rain":
+            case "vert heavy rain":
+            case "extreme rain":
+            case "shower rain":
+            case "heavy intensity shower rain":
+                return <img className="weather-icon" alt="description" src={HeavyRain} label="clear sky" />;
             case "few clouds":
             case "scattered clouds":
                 return <img className="weather-icon" alt="description" src={PartlyCloudy} label="few clouds" />
