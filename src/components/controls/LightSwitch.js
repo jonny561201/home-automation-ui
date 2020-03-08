775c5ad90487ba5b18361bd0e3f32c1658056ffe
Jon Graf <jgraf@leantechniques.com>
2020-03-08 15:27:42 -0500

    moved components to a new folder and added new switches for each light in a group

---
+++ src/components/controls/LightSwitch.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { FormControlLabel, Switch } from '@material-ui/core';
+import { setLightGroupState } from '../../utilities/RestApi';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import './LightSwitch.css';
+
+export default class LightSwitch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            areLightsOpen: false,
+            on: this.props.data.on,
+            groupId: this.props.data.groupId,
+            groupName: this.props.data.groupName,
+            brightness: this.props.data.brightness,
+            lights: this.props.data.lights,
+            // Store the lights off the group
+        }
+    }
+
+    toggleChecked = async () => {
+        await this.setState({ on: !this.state.on });
+        await setLightGroupState(this.state.groupId, this.state.on, this.state.brightness);
+    };
+
+    toggleOpen = () => {
+        this.setState({ areLightsOpen: !this.state.areLightsOpen });
+    };
+
+    getLightSwitches = () => {
+        return this.state.lights.map(x => <FormControlLabel className="test-class" control={<Switch checked={x.on} />}
+            label={x.lightName} />)
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="light-group">
+                    <ButtonBase onClick={() => this.toggleOpen()} style={{ "border-radius": "50%" }}>
+                        <div className={`light-chevron ${this.state.areLightsOpen ? "expanded" : ""}`}>
+                            <ChevronRightIcon />
+                        </div>
+                    </ButtonBase>
+                    <FormControlLabel
+                        control={<Switch checked={this.state.on} onChange={this.toggleChecked} color="primary" />}
+                        label={this.state.groupName}
+                    />
+                </div>
+                {this.state.areLightsOpen ?
+                    <div className="light-group-expansion">{this.getLightSwitches()}</div>
+                    :
+                    null}
+            </div>
+        );
+    }
+}
--- src/tests/components/LightSwitch.spec.js
+++ src/tests/components/LightSwitch.spec.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import * as lib from '../../utilities/RestApi';
-import LightSwitch from '../../components/LightSwitch';
+import LightSwitch from '../../components/controls/LightSwitch';
 
 describe('LightSwitch', () => {
     let lightSwitch;
     const spySet = jest.spyOn(lib, 'setLightGroupState');
-    const groupData = { 'groupId': '1', 'groupName': 'DinningRoom', 'on': true, 'brightness': 155 }
+    const groupData = {
+        'groupId': '1', 'groupName': 'DinningRoom', 'on': true,
+        'brightness': 155, 'lights': [{ 'lightName': 'desk lamp' }, { 'lightName': 'table lamp' }]
+    }
 
     beforeEach(() => {
         spySet.mockClear();
@@ -23,5 +26,41 @@ describe('LightSwitch', () => {
         lightSwitch.instance().toggleChecked();
 
         expect(spySet).toBeCalledWith(groupData.groupId, false, groupData.brightness);
+    });
+
+    it('should display the expansion icon', () => {
+        const actual = lightSwitch.find('ChevronRightIcon');
+
+        expect(actual).toHaveLength(1);
+    });
+
+    describe('Light Expansion', () => {
+
+        it('should display expansion panel when areLightsOpen is true', () => {
+            lightSwitch.state().areLightsOpen = true;
+            lightSwitch.instance().forceUpdate();
+
+            const actual = lightSwitch.find('.light-group-expansion');
+
+            expect(actual).toHaveLength(1);
+        });
+
+        it('should not display expansion panel when areLightsOpen is false', () => {
+            lightSwitch.state().areLightsOpen = false;
+            lightSwitch.instance().forceUpdate();
+
+            const actual = lightSwitch.find('.light-group-expansion');
+
+            expect(actual).toHaveLength(0);
+        });
+
+        it('should display all light switches', () => {
+            lightSwitch.state().areLightsOpen = true;
+            lightSwitch.instance().forceUpdate();
+
+            const actual = lightSwitch.find('.test-class');
+
+            expect(actual).toHaveLength(2);
+        });
     });
 })
