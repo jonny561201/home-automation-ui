d003c2a39e50e86287d7fd11697356e3191b5281
Jon Graf <jongraf1@gmail.com>
2020-04-05 12:46:50 -0500

    added ability to toggle between account, settings, and home pages

--- src/components/header/AccountSettings.js
+++ src/components/header/AccountSettings.js
@@ -5,6 +5,12 @@ import { getStore } from '../../GlobalState';
 
 
 export default class AccountSettings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: getStore().getActivePage()
+        }
+    }
 
     componentDidMount = () => {
         document.addEventListener("mousedown", this.handleClickOutside);
@@ -20,17 +26,43 @@ export default class AccountSettings extends React.Component {
         }
     }
 
+    getLinks = () => {
+        if (this.state.activePage === "Home Automation") {
+            return <div>
+                <Link to='/settings'>
+                    <li><div className="account-button">Settings</div></li>
+                </Link>
+                <Link to='/account'>
+                    <li><div className="account-button">Account</div></li>
+                </Link>
+            </div>
+        }
+        else if (this.state.activePage === "Settings") {
+            return <div>
+                <Link to='/home'>
+                    <li><div className="account-button">Home</div></li>
+                </Link>
+                <Link to='/account'>
+                    <li><div className="account-button">Account</div></li>
+                </Link>
+            </div>
+        }
+        else {
+            return <div>
+                <Link to='/home'>
+                    <li><div className="account-button">Home</div></li>
+                </Link>
+                <Link to='/settings'>
+                    <li><div className="account-button">Settings</div></li>
+                </Link>
+            </div>
+        }
+    }
+
     render() {
         return (
             <div className="account-menu" ref={this.setWrapperRef}>
-                <ul>{getStore().getActivePage() === 'Home Automation'
-                    ? <Link to='/settings'>
-                        <li><div className="account-button">Settings</div></li>
-                    </Link>
-                    : <Link to='/home'>
-                        <li><div className="account-button">Home</div></li>
-                    </Link>
-                }
+                <ul>{ this.getLinks() }
                     <Link to='/'>
                         <li><div className="account-button" onClick={() => getStore().updateAuth(false)}>Sign Out</div></li>
                     </Link>
--- src/tests/components/header/AccountSettings.spec.js
+++ src/tests/components/header/AccountSettings.spec.js
@@ -13,28 +13,42 @@ describe('AccountSettings', () => {
     });
 
     it('should display sign out link', () => {
-        const actual = accountSettings.find('.account-button').at(1).text();
+        const actual = accountSettings.find('.account-button').at(2).text();
         expect(actual).toEqual('Sign Out');
     });
 
-    it('should display Settings link when active page is set to home', () => {
+    it('should display Settings and Account links when active page is set to home', () => {
         getStore().setActivePage('Home Automation');
-        accountSettings.setProps({});
+        const accountSet = shallow(<AccountSettings />);
 
-        const actual = accountSettings.find('.account-button').at(0).text();
-        expect(actual).toEqual('Settings');
+        const settings = accountSet.find('.account-button').at(0).text();
+        const account = accountSet.find('.account-button').at(1).text();
+        expect(settings).toEqual('Settings');
+        expect(account).toEqual('Account');
     });
 
-    it('should display Home link when active page is set to settings', () => {
+    it('should display Home and Account links when active page is set to settings', () => {
         getStore().setActivePage('Settings');
-        accountSettings.setProps({});
+        const accountSet = shallow(<AccountSettings />);
 
-        const actual = accountSettings.find('.account-button').at(0).text();
-        expect(actual).toEqual('Home');
+        const home = accountSet.find('.account-button').at(0).text();
+        const account = accountSet.find('.account-button').at(1).text();
+        expect(home).toEqual('Home');
+        expect(account).toEqual('Account');
+    });
+
+    it('should display Home and Settings links when active page is set to Account', () => {
+        getStore().setActivePage('Account');
+        const accountSet = shallow(<AccountSettings />);
+
+        const settings = accountSet.find('.account-button').at(1).text();
+        const account = accountSet.find('.account-button').at(0).text();
+        expect(settings).toEqual('Settings');
+        expect(account).toEqual('Home');
     });
 
     it('should deauthenticate user when click sign out', () => {
-        const test = accountSettings.find('.account-button').at(1).simulate('click');
+        const test = accountSettings.find('.account-button').at(2).simulate('click');
         expect(test).toHaveLength(1);
         expect(getStore().isAuthenticated()).toBeFalsy();
     });
