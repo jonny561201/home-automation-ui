00bc216d6304bd84134660ec3f35148ab5a1e82b
Jon Graf <jongraf1@gmail.com>
2020-04-06 18:06:52 -0500

    added methods for the account page to validate and submit the change password request

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -2,35 +2,80 @@ import React from 'react';
 import Header from '../components/header/Header';
 import { Divider, TextField } from '@material-ui/core';
 import { getStore } from '../GlobalState';
+import { updateUserAccount } from '../utilities/RestApi';
 
-export default function Account() {
-    getStore().setActivePage('Account')
+export default class Account extends React.Component {
+    constructor(props) {
+        super(props)
+        getStore().setActivePage('Account')
+        this.state = {
+            changed: false,
+            doPasswordsMatch: null,
+            oldPasswordError: null,
+            oldPassword: null,
+            firstNewPassword: null,
+            secondNewPassword: null,
+        }
+    }
 
-    return (
-        <div>
-            <div className="settings-header">
-                <Header />
-            </div>
-            <div className="settings-body">
-                <div className="settings-wrapper">
-                    <div className="settings-group settings-text">
-                        <h2>Change Password</h2>
-                        <Divider />
-                        <div className="settings-row">
-                            <TextField variant="outlined" label="Old Password" type="password"/>
-                        </div>
-                        <div className="settings-row">
-                            <TextField variant="outlined" label="New Password" type="password"/>
-                        </div>
-                        <div className="settings-row">
-                            <TextField variant="outlined" label="Confirm New Password" type="password"/>
-                        </div>
-                        <Divider />
-                        <button>Submit</button>
-                    </div>
+    onOldPasswordChange = async (input) => {
+        await this.setState({oldPassword: input.target.value, changed: true});
+        if (this.state.oldPassword === "" || this.state.oldPassword == null) {
+            this.setState({oldPasswordError: true})
+        } else {
+            this.setState({oldPasswordError: false});
+        }
+    }
+
+    onFirstPasswordChange = async (input) => {
+        await this.setState({firstNewPassword: input.target.value, changed: true});
+        if (this.state.firstNewPassword && this.state.secondNewPassword) {
+            this.setState({doPasswordsMatch: this.state.secondNewPassword !== this.state.firstNewPassword})
+        }
+    }
+
+    onSecondPasswordChange = async (input) => {
+        await this.setState({secondNewPassword: input.target.value, changed: true});
+        if (this.state.firstNewPassword && this.state.secondNewPassword) {
+            this.setState({doPasswordsMatch: this.state.secondNewPassword !== this.state.firstNewPassword})
+        }
+    }
+
+    submitAccountChange = () => {
+        if (this.state.changed && !this.state.oldPasswordError && !this.state.doPasswordsMatch) {
+            updateUserAccount(getStore().getUserId(), this.state.oldPassword, this.state.secondNewPassword)
+        } else {
+            this.setState({oldPasswordError: true, doPasswordsMatch: true})
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="settings-header">
+                    <Header />
                 </div>
+                <div className="settings-body">
+                    <div className="settings-wrapper">
+                        <div className="settings-group settings-text">
+                            <h2>Change Password</h2>
+                            <Divider />
+                            <div className="settings-row">
+                                <TextField error={this.state.oldPasswordError} value={this.state.oldPassword} variant="outlined" label="Old Password" type="password" onChange={this.onOldPasswordChange}/>
+                            </div>
+                            <div className="settings-row">
+                                <TextField error={this.state.doPasswordsMatch} value={this.state.firstNewPassword} variant="outlined" label="New Password" type="password" onChange={this.onFirstPasswordChange}/>
+                            </div>
+                            <div className="settings-row">
+                                <TextField error={this.state.doPasswordsMatch} value={this.state.secondNewPassword} variant="outlined" label="Confirm New Password" type="password" onChange={this.onSecondPasswordChange} />
+                            </div>
+                            <Divider />
+                            <button onClick={this.submitAccountChange}>Submit</button>
+                        </div>
+                    </div>
 
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- src/tests/pages/Account.spec.js
+++ src/tests/pages/Account.spec.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Account from '../../pages/Account';
+import * as lib from '../../utilities/RestApi';
 import { TextField } from '@material-ui/core';
+import { getStore } from '../../GlobalState';
 
 describe('Account Page', () => {
 
     let account;
+    const spyPost = jest.spyOn(lib, 'updateUserAccount');
+    const userId = 'fakeUserId'
 
     beforeEach(() => {
+        getStore().setUserId(userId);
+        spyPost.mockClear();
         account = shallow(<Account />)
     });
 
@@ -50,5 +56,98 @@ describe('Account Page', () => {
     it('should display the submit button', () => {
         const actual = account.find('button').text();
         expect(actual).toEqual('Submit');
+    });
+
+    describe('SubmitAccountChange', () => {
+        const oldPass = "oldPassword";
+        const newPass = "newPassword";
+        
+        it('should call updateUserAccount when not in error state', () => {
+            account.state().oldPasswordError = false;
+            account.state().changed = true;
+            account.state().doPasswordsMatch = false;
+            account.state().oldPassword = oldPass;
+            account.state().secondNewPassword = newPass;
+            account.instance().forceUpdate();
+
+            account.instance().submitAccountChange();
+
+            expect(spyPost).toBeCalledWith(userId, oldPass, newPass)
+        });
+        
+        it('should set to error when state unchanged', () => {
+            account.state().oldPasswordError = false;
+            account.state().changed = false;
+            account.state().doPasswordsMatch = false;
+            account.state().oldPassword = oldPass;
+            account.state().secondNewPassword = newPass;
+            account.instance().forceUpdate();
+
+            account.instance().submitAccountChange();
+
+            expect(spyPost).toHaveBeenCalledTimes(0);
+            expect(account.state().oldPasswordError).toBeTruthy();
+            expect(account.state().doPasswordsMatch).toBeTruthy();
+        });
+        
+        it('should set to error when state old password error', () => {
+            account.state().oldPasswordError = true;
+            account.state().changed = true;
+            account.state().doPasswordsMatch = false;
+            account.state().oldPassword = oldPass;
+            account.state().secondNewPassword = newPass;
+            account.instance().forceUpdate();
+
+            account.instance().submitAccountChange();
+
+            expect(spyPost).toHaveBeenCalledTimes(0);
+            expect(account.state().oldPasswordError).toBeTruthy();
+            expect(account.state().doPasswordsMatch).toBeTruthy();
+        });
+        
+        it('should set to error when state password error', () => {
+            account.state().oldPasswordError = false;
+            account.state().changed = true;
+            account.state().doPasswordsMatch = true;
+            account.state().oldPassword = oldPass;
+            account.state().secondNewPassword = newPass;
+            account.instance().forceUpdate();
+
+            account.instance().submitAccountChange();
+
+            expect(spyPost).toHaveBeenCalledTimes(0);
+            expect(account.state().oldPasswordError).toBeTruthy();
+            expect(account.state().doPasswordsMatch).toBeTruthy();
+        });
+    });
+
+    describe('onSecondPasswordChange', () => {
+        const firstPass ="firstPassword";
+        const secondPass = "secondPassword";
+
+        it('should update the second password and update the changed value', () => {
+            
+            await account.instance().onSecondPasswordChange(secondPass);
+            expect(account.state().secondNewPassword).toEqual(secondPass);
+            expect(account.state().changed).toBeTruthy();
+        });
+
+        it('should set the do passwords match to false when match', () => {
+            account.state().firstNewPassword = firstPass;
+            account.state().secondNewPassword = firstPass;
+            account.instance().forceUpdate();
+
+            await account.instance().onSecondPasswordChange(secondPass);
+            expect(account.state().doPasswordsMatch).toBeFalsy();
+        });
+
+        it('should set the do passwords match to true when dont match', () => {
+            account.state().firstNewPassword = firstPass;
+            account.state().secondNewPassword = secondPass;
+            account.instance().forceUpdate();
+
+            await account.instance().onSecondPasswordChange(secondPass);
+            expect(account.state().doPasswordsMatch).toBeTruthy();
+        });
     });
 });
