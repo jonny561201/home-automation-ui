9ca6d71a15d9ffb6fe71be974026300cd16e6240
Jon Graf <jongraf1@gmail.com>
2020-04-08 21:45:04 -0500

    added success and failure message when the username update is submitted

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import { Divider, TextField } from '@material-ui/core';
+import { CheckCircle, Error } from '@material-ui/icons';
 import { getStore } from '../GlobalState';
 import { updateUserAccount } from '../utilities/RestApi';
 import './Account.css';
@@ -16,37 +17,55 @@ export default class Account extends React.Component {
             oldPassword: null,
             firstNewPassword: null,
             secondNewPassword: null,
+            succeeded: null,
         }
     }
 
     onOldPasswordChange = async (input) => {
-        await this.setState({oldPassword: input.target.value, changed: true});
+        await this.setState({ oldPassword: input.target.value, changed: true });
         if (this.state.oldPassword === "" || this.state.oldPassword == null) {
-            this.setState({oldPasswordError: true})
+            this.setState({ oldPasswordError: true })
         } else {
-            this.setState({oldPasswordError: false});
+            this.setState({ oldPasswordError: false });
         }
     }
 
     onFirstPasswordChange = async (input) => {
-        await this.setState({firstNewPassword: input.target.value, changed: true});
+        await this.setState({ firstNewPassword: input.target.value, changed: true });
         if (this.state.firstNewPassword && this.state.secondNewPassword) {
-            this.setState({arePasswordsMismatched: this.state.secondNewPassword !== this.state.firstNewPassword})
+            this.setState({ arePasswordsMismatched: this.state.secondNewPassword !== this.state.firstNewPassword })
         }
     }
 
     onSecondPasswordChange = async (input) => {
-        await this.setState({secondNewPassword: input.target.value, changed: true});
+        await this.setState({ secondNewPassword: input.target.value, changed: true });
         if (this.state.firstNewPassword && this.state.secondNewPassword) {
-            this.setState({arePasswordsMismatched: this.state.secondNewPassword !== this.state.firstNewPassword})
+            this.setState({ arePasswordsMismatched: this.state.secondNewPassword !== this.state.firstNewPassword })
         }
     }
 
-    submitAccountChange = () => {
+    submitAccountChange = async () => {
         if (this.state.changed && !this.state.oldPasswordError && !this.state.arePasswordsMismatched) {
-            updateUserAccount(getStore().getUserId(), this.state.oldPassword, this.state.secondNewPassword)
+            const response = await updateUserAccount(getStore().getUserId(), this.state.oldPassword, this.state.secondNewPassword);
+            this.setState({ succeeded: response.ok });
         } else {
-            this.setState({oldPasswordError: true, arePasswordsMismatched: true})
+            this.setState({ oldPasswordError: true, arePasswordsMismatched: true })
+        }
+    }
+
+    passwordMessage = () => {
+        if (this.state.succeeded) {
+            return <div className="account-message">
+                <CheckCircle className="success-text" />
+                <p className="success-text">Updated Successfully!</p>
+            </div>
+        } else if (this.state.succeeded === false) {
+            return <div className="account-message">
+                <Error className="failure-text"/>
+                <p className="failure-text">Password Update Failed</p>
+            </div>
+        } else {
+            return <div><p></p></div>
         }
     }
 
@@ -62,15 +81,16 @@ export default class Account extends React.Component {
                             <h2>Change Password</h2>
                             <Divider />
                             <div className="account-row">
-                                <TextField error={this.state.oldPasswordError} value={this.state.oldPassword} variant="outlined" label="Old Password" type="password" onChange={this.onOldPasswordChange}/>
+                                <TextField error={this.state.oldPasswordError} value={this.state.oldPassword} variant="outlined" label="Old Password" type="password" onChange={this.onOldPasswordChange} />
                             </div>
                             <div className="account-row">
-                                <TextField error={this.state.arePasswordsMismatched} value={this.state.firstNewPassword} variant="outlined" label="New Password" type="password" onChange={this.onFirstPasswordChange}/>
+                                <TextField error={this.state.arePasswordsMismatched} value={this.state.firstNewPassword} variant="outlined" label="New Password" type="password" onChange={this.onFirstPasswordChange} />
                             </div>
                             <div className="account-row">
                                 <TextField error={this.state.arePasswordsMismatched} value={this.state.secondNewPassword} variant="outlined" label="Confirm New Password" type="password" onChange={this.onSecondPasswordChange} />
                             </div>
-                            <Divider className="account-divider"/>
+                            {this.passwordMessage()}
+                            <Divider className="account-divider" />
                             <button onClick={this.submitAccountChange}>Submit</button>
                         </div>
                     </div>
