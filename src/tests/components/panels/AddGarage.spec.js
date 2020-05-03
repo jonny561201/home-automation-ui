d42bd6244957517249a317fd5687e85b9ddea39e
Jon Graf <jongraf1@gmail.com>
2020-05-03 17:33:41 -0500

    added test to make api call when user input is valid

--- src/tests/components/panels/AddGarage.spec.js
+++ src/tests/components/panels/AddGarage.spec.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AddGarage from "../../../components/panels/AddGarage";
+import { getStore } from '../../../GlobalState';
+import * as lib from '../../../utilities/RestApi';
 
 describe('Add Garage', () => {
+
+    const userId = 'fakeUserId';
+    getStore().setUserId(userId);
+    const spyAdd = jest.spyOn(lib, 'addUserDeviceNode');
+
+    beforeEach(() => {
+        spyAdd.mockClear();
+    });
 
     it('should display the Add Garage Door text', () => {
         render(<AddGarage />);
@@ -20,5 +30,13 @@ describe('Add Garage', () => {
         render(<AddGarage />);
         const actual = screen.getByRole('button').textContent;
         expect(actual).toEqual('Add');
+    });
+
+    it('should make api call if the when valid name', () => {
+        render(<AddGarage />);
+        const name = 'TestGarage';
+        fireEvent.change(screen.getByRole('textbox'), {target: {value: name}});
+        fireEvent.click(screen.getByRole('button'));
+        expect(spyAdd).toBeCalledWith(userId, 'deviceId', name);
     });
 });
