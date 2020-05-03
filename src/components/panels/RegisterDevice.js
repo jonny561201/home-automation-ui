86ce4be6ba9454458c912a9d5448c8cb1cc5c7a3
Jon Graf <jongraf1@gmail.com>
2020-05-02 23:18:19 -0500

    turned add devices into floating menu item...needs refactor to show menu

--- src/components/panels/RegisterDevice.js
+++ src/components/panels/RegisterDevice.js
@@ -3,34 +3,35 @@ import { Divider, TextField } from '@material-ui/core';
 import { isValidIpAddress, debounchApi } from '../../utilities/Services';
 import { addUserDevice } from '../../utilities/RestApi';
 import { getStore } from '../../GlobalState';
+import './RegisterDevice.css';
 
 export default function RegisterDevice() {
     const [isIpValid, setIsIpValid] = useState(true);
     const [ipAddress, setIpAddress] = useState();
-
+    const [touched, setTouched] = useState(false);
 
     const checkIpAddress = (input) => {
         const ipAddress = input.target.value;
         debounchApi(() => setIsIpValid(isValidIpAddress(ipAddress)));
         setIpAddress(ipAddress);
+        setTouched(true);
     }
 
     const submitDevice = (event) => {
         event.preventDefault();
-        if(isIpValid){
+        if(isIpValid && touched){
             addUserDevice(getStore().getUserId(), 'garage_door', ipAddress)
         }
     }
 
     return (
-        <div>
+        <div className="device-menu device-text">
             <h2 data-testid={"data-add-device"}>Add Device</h2>
-            <Divider />
             <form  onSubmit={submitDevice }>
                 <div className="account-row">
-                    <TextField value={ipAddress} error={!isIpValid} onChange={checkIpAddress} variant="outlined" label="Add Device" />
+                    <TextField value={ipAddress} error={!isIpValid} onChange={checkIpAddress} variant="outlined" label="IP Address" />
                 </div>
-                <button type="submit">Add</button>
+                <button type="submit">Next</button>
             </form>
         </div>
     );
