8d86b3402b4c3b64e1644e34a7a71854f87f29ae
Jon Graf <jongraf1@gmail.com>
2020-05-08 19:00:22 -0500

    added test coverage for showing the success screen on the add garage panel

--- src/components/panels/AddGarage.js
+++ src/components/panels/AddGarage.js
@@ -3,38 +3,78 @@ import { TextField } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { addUserDeviceNode } from '../../utilities/RestApi';
 import { getStore } from '../../GlobalState';
+import { CheckCircle } from '@material-ui/icons';
+import './AddGarage.css';
 
 export default function AddGarage(props) {
+    const [succeeded, setSucceeded] = useState();
     const [garageName, setGarageName] = useState('');
     const [isNameValid, setIsNameValid] = useState(true);
     const [garageTouched, setGarageTouched] = useState(false);
+    const [availableNodes, setAvailableNodes] = useState(1);
 
     const checkGarageName = (input) => {
-        const name = input.target.value;
-        setGarageTouched(true);
-        setIsNameValid(name !== "");
-        setGarageName(name);
+        if (availableNodes > 0) {
+            const name = input.target.value;
+            setGarageTouched(true);
+            setIsNameValid(name !== "");
+            setGarageName(name);
+        } else {
+            props.close();
+        }
     }
 
     const submitGarageDoor = async (event) => {
         event.preventDefault();
         (garageTouched && isNameValid)
-            ? addUserDeviceNode(getStore().getUserId(), props.deviceId, garageName)
+            ? updateGarageNode()
             : setIsNameValid(false);
+    }
+
+    const updateGarageNode = async () => {
+        const response = await addUserDeviceNode(getStore().getUserId(), props.deviceId, garageName);
+        setSucceeded(response.ok);
+        const jsonResponse = await response.json();
+        setAvailableNodes(jsonResponse.availableNodes);
+        console.log('RESPONSE:', succeeded)
+        if (jsonResponse.availableNodes === 0 ) {props.close();}
+    }
+
+    const resetDevices = () => {
+        setSucceeded(false);
+        setGarageName('');
+        setGarageTouched(false);
     }
 
     return (
         <div>
-            <div className="device-group">
-                <h2 data-testid={"data-add-device"} className=" device-text">Add Garage Door</h2>
-                <CloseIcon data-testid={"garage-close-button"} onClick={() => props.close()} className="close-icon" />
-            </div>
-            <form onSubmit={submitGarageDoor}>
-                <div className="account-row">
-                    <TextField value={garageName} error={!isNameValid} onChange={checkGarageName} variant="outlined" label="Garage Name" />
+            {succeeded
+                ? <div>
+                    <div className="device-group">
+                        <div className="device-group">
+                            <CheckCircle className="success-text" />
+                            <h2 data-testid={"data-add-device"} className="device-text success-text">Successfully Added</h2>
+                        </div>
+                        <CloseIcon data-testid={"garage-close-button"} onClick={() => props.close()} className="close-icon" />
+                    </div>
+                    <div className="device-row">
+                        <p className="device-text">Would you like to setup the remaining ({availableNodes}) openers?</p>
+                    </div>
+                    <button onClick={resetDevices}>Add</button>
                 </div>
-                <button type="submit">Add</button>
-            </form>
+                : <div>
+                    <div className="device-group">
+                        <h2 data-testid={"data-add-device"} className=" device-text">Add Garage Door</h2>
+                        <CloseIcon data-testid={"garage-close-button"} onClick={() => props.close()} className="close-icon" />
+                    </div>
+                    <form onSubmit={submitGarageDoor}>
+                        <div className="account-row">
+                            <TextField value={garageName} error={!isNameValid} onChange={checkGarageName} variant="outlined" label="Garage Name" />
+                        </div>
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
+            }
         </div>
     );
 }
--- src/tests/components/panels/AddGarage.spec.js
+++ src/tests/components/panels/AddGarage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act, waitForElement } from '@testing-library/react';
 import AddGarage from "../../../components/panels/AddGarage";
 import { getStore } from '../../../GlobalState';
 import * as lib from '../../../utilities/RestApi';
@@ -14,50 +14,69 @@ describe('Add Garage', () => {
         spyAdd.mockClear();
     });
 
-    it('should display the Add Garage Door text', () => {
-        render(<AddGarage />);
-        const actual = screen.getByRole('heading').textContent;
-        expect(actual).toEqual('Add Garage Door');
+    describe('Add Device Screen', () => {
+        it('should display the Add Garage Door text', () => {
+            render(<AddGarage />);
+            const actual = screen.getByRole('heading').textContent;
+            expect(actual).toEqual('Add Garage Door');
+        });
+
+        it('should display the Garage Door input box', () => {
+            render(<AddGarage />);
+            const actual = screen.getByRole('textbox');
+            expect(actual).toBeDefined();
+        });
+
+        it('should display the close icon', () => {
+            render(<AddGarage />);
+            const actual = screen.getByTestId('garage-close-button');
+            expect(actual).toBeDefined();
+        });
+
+        it('should display the Add garage button', () => {
+            render(<AddGarage />);
+            const actual = screen.getByRole('button').textContent;
+            expect(actual).toEqual('Add');
+        });
+
+        it('should make api call if the when valid name', () => {
+            const deviceId = 'testDeviceId';
+            render(<AddGarage deviceId={deviceId}/>);
+            const name = 'TestGarage';
+            fireEvent.change(screen.getByRole('textbox'), {target: {value: name}});
+            fireEvent.click(screen.getByRole('button'));
+            expect(spyAdd).toBeCalledWith(userId, deviceId, name);
+        });
+
+        it('should not make api call if the when invalid name', () => {
+            render(<AddGarage />);
+            const name = '';
+            fireEvent.change(screen.getByRole('textbox'), {target: {value: name}});
+            fireEvent.click(screen.getByRole('button'));
+            expect(spyAdd).toHaveBeenCalledTimes(0);
+        });
+
+        it('should not make api call if the when name is untouched', () => {
+            render(<AddGarage />);
+            fireEvent.click(screen.getByRole('button'));
+            expect(spyAdd).toHaveBeenCalledTimes(0);
+        });
     });
 
-    it('should display the Garage Door input box', () => {
-        render(<AddGarage />);
-        const actual = screen.getByRole('textbox');
-        expect(actual).toBeDefined();
-    });
+    describe('Add Node Screen', () => {
 
-    it('should display the close icon', () => {
-        render(<AddGarage />);
-        const actual = screen.getByTestId('garage-close-button');
-        expect(actual).toBeDefined();
-    });
-
-    it('should display the Add garage button', () => {
-        render(<AddGarage />);
-        const actual = screen.getByRole('button').textContent;
-        expect(actual).toEqual('Add');
-    });
-
-    it('should make api call if the when valid name', () => {
-        const deviceId = 'testDeviceId';
-        render(<AddGarage deviceId={deviceId}/>);
-        const name = 'TestGarage';
-        fireEvent.change(screen.getByRole('textbox'), {target: {value: name}});
-        fireEvent.click(screen.getByRole('button'));
-        expect(spyAdd).toBeCalledWith(userId, deviceId, name);
-    });
-
-    it('should not make api call if the when invalid name', () => {
-        render(<AddGarage />);
-        const name = '';
-        fireEvent.change(screen.getByRole('textbox'), {target: {value: name}});
-        fireEvent.click(screen.getByRole('button'));
-        expect(spyAdd).toHaveBeenCalledTimes(0);
-    });
-
-    it('should not make api call if the when name is untouched', () => {
-        render(<AddGarage />);
-        fireEvent.click(screen.getByRole('button'));
-        expect(spyAdd).toHaveBeenCalledTimes(0);
+        it('should display the Success Header', async () => {
+            spyAdd.mockReturnValue({ ok: true, json: () => {return {availableNodes: 1}} });
+            const name = "ImValid";
+            await act(async () => {
+                render(<AddGarage />);
+            });
+            fireEvent.change(screen.getByRole('textbox'), { target: { value: name } });
+            await act(async () => {
+                fireEvent.click(screen.getByRole('button'));
+            });
+            const actual = screen.getByText('Successfully Added').textContent;
+            expect(actual).toEqual('Successfully Added')
+        });
     });
 });
