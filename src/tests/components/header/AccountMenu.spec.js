a12d232dc129e0619ab6843f0d81373a22b1a2b3
Jon Graf <jongraf1@gmail.com>
2020-05-11 19:14:36 -0500

    cut over AccountMenu tests to use the react testing library

--- src/tests/components/header/AccountMenu.spec.js
+++ src/tests/components/header/AccountMenu.spec.js
@@ -1,55 +1,60 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { getStore } from '../../../state/GlobalState';
 import AccountMenu from '../../../components/header/AccountMenu';
+import { BrowserRouter } from 'react-router-dom';
 
 
 describe('AccountSettings', () => {
-    let accountMenu;
 
-    beforeEach(() => {
-        getStore().updateAuth(true);
-        accountMenu = shallow(<AccountMenu />);
-    });
+    const renderComponent = () => {
+        render(
+            <BrowserRouter>
+                <AccountMenu />
+            </BrowserRouter>
+        );
+    };
 
     it('should display sign out link', () => {
-        const actual = accountMenu.find('.account-button').at(2).text();
+        renderComponent();
+        const actual = screen.getByText('Sign Out').textContent;
         expect(actual).toEqual('Sign Out');
     });
 
     it('should display Settings and Account links when active page is set to home', () => {
         getStore().setActivePage('Home Automation');
-        const accountSet = shallow(<AccountMenu />);
+        renderComponent();
 
-        const settings = accountSet.find('.account-button').at(0).text();
-        const account = accountSet.find('.account-button').at(1).text();
+        const settings = screen.getByText('Settings').textContent;
+        const account = screen.getByText('Account').textContent;
         expect(settings).toEqual('Settings');
         expect(account).toEqual('Account');
     });
 
     it('should display Home and Account links when active page is set to settings', () => {
         getStore().setActivePage('Settings');
-        const accountSet = shallow(<AccountMenu />);
+        renderComponent();
 
-        const home = accountSet.find('.account-button').at(0).text();
-        const account = accountSet.find('.account-button').at(1).text();
+        const home = screen.getByText('Home').textContent;
+        const account = screen.getByText('Account').textContent;
         expect(home).toEqual('Home');
         expect(account).toEqual('Account');
     });
 
     it('should display Home and Settings links when active page is set to Account', () => {
         getStore().setActivePage('Account');
-        const accountSet = shallow(<AccountMenu />);
+        renderComponent();
 
-        const settings = accountSet.find('.account-button').at(1).text();
-        const account = accountSet.find('.account-button').at(0).text();
+        const settings = screen.getByText('Settings').textContent;
+        const home = screen.getByText('Home').textContent;
         expect(settings).toEqual('Settings');
-        expect(account).toEqual('Home');
+        expect(home).toEqual('Home');
     });
 
     it('should deauthenticate user when click sign out', () => {
-        const test = accountMenu.find('.account-button').at(2).simulate('click');
-        expect(test).toHaveLength(1);
+        renderComponent();
+        const signOut = screen.getByText('Sign Out');
+        fireEvent.click(signOut);
         expect(getStore().isAuthenticated()).toBeFalsy();
     });
 });
