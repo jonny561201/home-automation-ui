e0fa4df2a0448bd7f912cc7454d70608fd240442
Jon Graf <jongraf1@gmail.com>
2020-05-16 16:38:54 -0500

    migrated the settings panel non-edit mode to a separate component

---
+++ src/components/panels/SettingsPanel.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Divider } from '@material-ui/core';
+
+
+export default function SettingsPanel(props) {
+    return (
+        <div className="settings-wrapper">
+            <div className="settings-group settings-text">
+                <h2>Temperature</h2>
+            </div>
+            <Divider />
+            <div className="settings-row">
+                <p className="settings-text temp-unit">Unit:</p>
+                <p className="settings-text temp-unit">{props.tempUnit}</p>
+            </div>
+            <div className="settings-row">
+                <p className="settings-text temp-city">City:</p>
+                <p className="settings-text temp-city">{props.city}</p>
+            </div>
+            <div className="settings-wrapper settings-text">
+                <h2>Measurement</h2>
+            </div>
+            <Divider />
+            <div className="settings-row">
+                <p className="settings-text measure-unit">Unit:</p>
+                <p className="settings-text measure-unit">{props.measureUnit}</p>
+            </div>
+            <Divider />
+            <button onClick={props.toggleEdit}>Edit</button>
+        </div>
+    );
+}
---
+++ src/tests/components/panels/SettingsPanel.spec.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import SettingsPanel from '../../../components/panels/SettingsPanel';
+
+
+describe('Settings Panel', () => {
+
+    it('should display edit button', () => {
+        render(<SettingsPanel />);
+        const actual = screen.getByRole('button').textContent;
+        expect(actual).toEqual('Edit');
+    });
+
+    it('should display the unit text for temperature', () => {
+        render(<SettingsPanel />);
+        const actual = screen.getAllByText('Unit:')[0].textContent;
+        expect(actual).toEqual('Unit:');
+    });
+
+    it('should display the fahrenheit setting stored in state', () => {
+        const tempUnit = 'fahrenheit'
+        render(<SettingsPanel tempUnit={tempUnit}/>);
+        const actual = screen.getByText(tempUnit).textContent;
+        expect(actual).toEqual(tempUnit);
+    });
+
+    it('should display the city text for temperature', () => {
+        render(<SettingsPanel />);
+        const actual = screen.getByText('City:').textContent;
+        expect(actual).toEqual('City:');
+    });
+
+    it('should display the currently city setting stored in state', () => {
+        const city = 'Vienna';
+        render(<SettingsPanel city={city} />);
+        const actual = screen.getByText(city).textContent;
+        expect(actual).toEqual(city);
+    });
+
+    it('should display Temperature header', () => {
+        render(<SettingsPanel />);
+        const actual = screen.getByText('Temperature').textContent;
+        expect(actual).toEqual('Temperature');
+    });
+
+    it('should display the Measurement header', () => {
+        render(<SettingsPanel />);
+        const actual = screen.getByText('Measurement').textContent;
+        expect(actual).toEqual('Measurement');
+    });
+
+    it('should display the unit text for measurement', () => {
+        render(<SettingsPanel />);
+        const actual = screen.getAllByText('Unit:')[1].textContent;
+        expect(actual).toEqual('Unit:');
+    });
+
+    it('should display the measurement unit stored in state', () => {
+        const measureUnit = 'imperial';
+        render(<SettingsPanel measureUnit={measureUnit}/>);
+        const actual = screen.getByText(measureUnit).textContent;
+        expect(actual).toEqual(measureUnit);
+    });
+
+});
