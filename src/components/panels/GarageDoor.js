0935149b9de3571a35c8cabc67092d5d7bef3f8b
Jon Graf <jongraf1@gmail.com>
2020-05-20 22:24:07 -0500

    broken out new components for the individual garage door elements and fixed interval issue

---
+++ src/components/panels/GarageDoor.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { Context } from '../../state/Store';
+import { toggleGarageDoor, updateGarageState, getGarageStatus } from '../../utilities/RestApi';
+import { ExpansionPanelDetails, ExpansionPanelActions } from '@material-ui/core';
+
+
+export default function GarageDoor(props) {
+    const [state, ] = useContext(Context);
+    const [isOpen, setIsOpen] = useState();
+    const [duration, setDuration] = useState(props.duration);
+    const [statusDays, setStatusDays] = useState(0);
+    const [statusMins, setStatusMins] = useState(0);
+    const [statusHours, setStatusHours] = useState(0);
+
+    useEffect(() => {
+        const getData = async () => {
+            const garageStatus = await getGarageStatus(state.userId, props.device.node_device);
+            setIsOpen(garageStatus.isGarageOpen);
+            setDuration(garageStatus.statusDuration);
+        };
+        getData();
+        const interval = setInterval(() => {
+            const diffMs = new Date() - new Date(duration);
+            setStatusDays(Math.floor(diffMs / 86400000));
+            setStatusHours(Math.floor((diffMs % 86400000) / 3600000));
+            setStatusMins(Math.round(((diffMs % 86400000) % 3600000) / 60000));
+          }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [duration, isOpen]);
+
+    return (
+        <div>
+            <ExpansionPanelDetails className="center">
+                <div className="status-text-group">
+                    <p className="door-status">Door: </p>
+                    <p className="status-text-bold">{props.device.node_name}</p>
+                </div>
+                <div className="status-text-group">
+                    <p className="door-status">Status: </p>
+                    {isOpen
+                        ? <p className="status-text-bold close">Open</p>
+                        : <p className="status-text-bold open">Closed</p>}
+                </div>
+                <div className="status-text-group">
+                    <p className="door-status">Duration: </p>
+                    {statusDays === 0
+                        ? <div />
+                        : <p className="status-text-bold">{statusDays}Days</p>}
+                    <p className="status-text-bold">{statusHours}Hr</p>
+                    {statusDays === 0
+                        ? <p className="status-text-bold">{statusMins}Min</p>
+                        : <div />}
+                </div>
+            </ExpansionPanelDetails>
+            <ExpansionPanelActions>
+                {isOpen
+                    ? <button data-testid={"update-garage-close"} className="close-button" onClick={() => updateGarageState(false, state.userId, props.device.node_device)}>Close</button>
+                    : <button data-testid={"update-garage-open"} className="open-button" onClick={() => updateGarageState(true, state.userId, props.device.node_device)}>Open</button>}
+                <button data-testid={"toggle-garage-button"} className="toggle-button" onClick={() => toggleGarageDoor(state.userId, props.device.node_device)}>Toggle</button>
+            </ExpansionPanelActions>
+        </div>
+    );
+}
---
+++ src/tests/components/panels/GarageDoor.spec.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen, act } from '@testing-library/react';
+import * as lib from '../../../utilities/RestApi';
+import GarageDoor from "../../../components/panels/GarageDoor";
+import { Context } from '../../../state/Store';
+
+
+describe('Garage Door', () => {
+    const userId = 'fakeuserid';
+    const doorName = 'testNode';
+    const device = {node_name: doorName, node_device: 1}
+    const spyGet = jest.spyOn(lib, 'getGarageStatus');
+    const spyUpdate = jest.spyOn(lib, 'updateGarageState');
+    const spyToggle = jest.spyOn(lib, 'toggleGarageDoor');
+
+    const renderComponent = async () => {
+        await act(async () => {
+            render(
+                <Context.Provider value={[{userId: userId}, () => {}]}>
+                    <GarageDoor device={device}/>
+                </Context.Provider>
+            );
+        });
+    }
+
+    beforeEach(() => {
+        spyGet.mockReturnValue({ isGarageOpen: true });
+        spyGet.mockClear();
+        spyUpdate.mockClear();
+        spyToggle.mockClear();
+    });
+
+
+    it('should show close garage button', async () => {
+        spyGet.mockReturnValue({isGarageOpen: true});
+        await renderComponent();
+        const actual = screen.getByTestId('update-garage-close').textContent;
+        expect(actual).toEqual('Close');
+    });
+
+    it('should show open garage button', async () => {
+        spyGet.mockReturnValue({isGarageOpen: false});
+        await renderComponent();
+        const actual = screen.getByTestId('update-garage-open').textContent;
+        expect(actual).toEqual('Open');
+    });
+
+    it('should show toggle garage button', async () => {
+        await renderComponent();
+        const actual = screen.getByTestId('toggle-garage-button').textContent;
+        expect(actual).toEqual('Toggle');
+    });
+
+    it('should show Door text', async () => {
+        await renderComponent();
+        const actual = screen.getByText('Door:').textContent;
+        expect(actual).toEqual('Door: ');
+    });
+
+    it('should show Door Name text', async () => {
+        await renderComponent();
+        const actual = screen.getByText(doorName).textContent;
+        expect(actual).toEqual(doorName);
+    });
+
+    it('should show Door Status text', async () => {
+        await renderComponent();
+        const actual = screen.getByText('Status:').textContent;
+        expect(actual).toEqual('Status: ');
+    });
+
+    it('should show Duration text', async () => {
+        await renderComponent();
+        const actual = screen.getByText('Duration:').textContent;
+        expect(actual).toEqual('Duration: ');
+    });
+
+    it('should display Close text when response is false', async () => {
+        spyGet.mockReturnValue({ isGarageOpen: false })
+        await renderComponent();
+        const actual = screen.getByText("Closed").textContent;
+        expect(actual).toEqual('Closed');
+    });
+
+    it('should display Open text when response is true', async () => {
+        spyGet.mockReturnValue({ isGarageOpen: true })
+        await renderComponent();
+        const actual = screen.getByText("Open").textContent;
+        expect(actual).toEqual('Open');
+    });
+
+    describe('garage door api', () => {
+
+        it('should make the initial call to get the garage data', async () => {
+            await renderComponent();
+            expect(spyGet).toBeCalledWith(userId, 1);
+        });
+
+        it('should call update function with false when closing', async () => {
+            spyGet.mockReturnValue({ isGarageOpen: true })
+            await renderComponent();
+            userEvent.click(screen.getByTestId("update-garage-close"));
+            expect(spyUpdate).toBeCalledWith(false, userId, 1);
+        });
+
+        it('should call update function with true when opening', async () => {
+            spyGet.mockReturnValue({ isGarageOpen: false })
+            await renderComponent();
+            userEvent.click(screen.getByTestId("update-garage-open"));
+            expect(spyUpdate).toBeCalledWith(true, userId, 1);
+        });
+        
+        it('should call toggle function', async () => {
+            await renderComponent();
+            userEvent.click(screen.getByTestId("toggle-garage-button"));
+
+            expect(spyToggle).toBeCalledWith(userId, 1);
+        });
+    });
+});
