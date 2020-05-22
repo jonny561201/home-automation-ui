932cff043b04323e45f92b05072a503763b242f7
Jon Graf <jgraf@leantechniques.com>
2020-05-22 12:49:49 -0500

    removed unnecessary component for testing

--- src/tests/components/header/UserPass.spec.js
+++ src/tests/components/header/UserPass.spec.js
@@ -9,11 +9,9 @@ import UserPass from '../../../components/header/UserPass';
 const renderComponent = async () => {
     await act(async () => {
         render(
-            <App>
-                <Context.Provider value={[{}, () => {}]}>
-                    <UserPass />
-                </Context.Provider>
-            </App>
+            <Context.Provider value={[{}, () => { }]}>
+                <UserPass />
+            </Context.Provider>
         );
     });
 }
@@ -118,7 +116,7 @@ describe('UserPass', () => {
         it('should navigate to home screen on successful login', async () => {
             const userName = 'validFirst';
             const password = 'validPass';
-            const bearerToken = {bearerToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiJlOTdmZWJjMC1mZDEwLTExZTktOGYwYi0zNjJiOWUxNTU2NjciLCJyb2xlcyI6W3sicm9sZV9uYW1lIjoidGhlcm1vc3RhdCJ9LHsicm9sZV9uYW1lIjoic2VjdXJpdHkifSx7InJvbGVfbmFtZSI6ImdhcmFnZV9kb29yIn0seyJyb2xlX25hbWUiOiJsaWdodGluZyJ9LHsicm9sZV9uYW1lIjoic3VtcF9wdW1wIn1dLCJmaXJzdF9uYW1lIjoiSm9uIiwibGFzdF9uYW1lIjoiVGVzdGVyIn0sImV4cCI6MTU4ODQ4MTU2NX0.SJhFh7v2Xf3d85vQj3Aop4tBYXJlI7_pQvvinwxti0M"};
+            const bearerToken = { bearerToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7InVzZXJfaWQiOiJlOTdmZWJjMC1mZDEwLTExZTktOGYwYi0zNjJiOWUxNTU2NjciLCJyb2xlcyI6W3sicm9sZV9uYW1lIjoidGhlcm1vc3RhdCJ9LHsicm9sZV9uYW1lIjoic2VjdXJpdHkifSx7InJvbGVfbmFtZSI6ImdhcmFnZV9kb29yIn0seyJyb2xlX25hbWUiOiJsaWdodGluZyJ9LHsicm9sZV9uYW1lIjoic3VtcF9wdW1wIn1dLCJmaXJzdF9uYW1lIjoiSm9uIiwibGFzdF9uYW1lIjoiVGVzdGVyIn0sImV4cCI6MTU4ODQ4MTU2NX0.SJhFh7v2Xf3d85vQj3Aop4tBYXJlI7_pQvvinwxti0M" };
             spyGet.mockReturnValue(bearerToken);
             renderComponent();
             await act(async () => {
