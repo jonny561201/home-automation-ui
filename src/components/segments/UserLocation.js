c0bb2d7fe46d10dc92f31d9273449d4dfd308cf1
Jon Graf <jgraf@leantechniques.com>
2020-10-17 15:18:47 -0500

    WIP: added code to calculate the users location and if the garage door should be opened based off proximity

---
+++ src/components/segments/UserLocation.js
@@ -0,0 +1,63 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from '../../state/Store';
+import { calculateDistanceInMeters } from '../../utilities/Location';
+
+export default function UserLocation() {
+    const [state, dispatch] = useContext(Context);
+    const [distance, setDistance] = useState();
+    const [firstCheck, setFirstCheck] = useState(false);
+    const [secondCheck, setSecondCheck] = useState(false);
+    const [inGarage, setInGarage] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            calculateDistance();
+        }, 5000);
+        return () => {
+            clearInterval(interval);
+        };
+    });
+
+    const shouldOpenGarage = () => {
+        //within 100M of Garage
+        //then within 50M of Garage within 10s
+        //then within 20M of Garage within 20s
+
+        //if garage door already in garage shouldnt open
+        //time duration to reset location checks
+        if (distance <= 5 && !firstCheck) {
+            setInGarage(true);
+        } else if (distance <= 15 && secondCheck && firstCheck && !inGarage) {
+            setInGarage(true);
+            return true;
+        } else if (distance <= 50 && firstCheck && !inGarage) {
+            setSecondCheck(true);
+            return false;
+        } else if (distance <= 100 && !inGarage) {
+            setFirstCheck(true);
+            return false;
+        } else {
+            setInGarage(false);
+            setFirstCheck(false);
+            setSecondCheck(false);
+            return false;
+        }
+    }
+
+    const calculateDistance = () => {
+        if (state.garageCoords !== null) {
+            navigator.geolocation.watchPosition((position) => {
+                console.log("updated location")
+                const coords = position.coords;
+                const currentCoords = state.garageCoords;
+                const userDistance = calculateDistanceInMeters(currentCoords.latitude, currentCoords.longitude, coords.latitude, coords.longitude);
+                setDistance(userDistance);
+                dispatch({ type: "SET_USER_COORDS", payload: userDistance });
+            }, (error) => { alert('Enable GPS position feature.') }, { enableHighAccuracy: true });
+        }
+    }
+
+    return (
+        <></>
+    )
+}
---
+++ src/utilities/Location.js
@@ -0,0 +1,18 @@
+export const calculateDistanceInMeters = (lat1, long1, lat2, long2) => {
+    const meterConversion = 6371e3;
+    const φ1 = lat2 * Math.PI / 180;
+    const φ2 = lat1 * Math.PI / 180;
+    const Δφ = (lat1 - lat2) * Math.PI / 180;
+    const Δλ = (long1 - long2) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return ((meterConversion * c) * 0.000621371).toFixed(2);
+}
+
+// export const shouldOpenGarage = (distance, interval) => {
+
+// }
