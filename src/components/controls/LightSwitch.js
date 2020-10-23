60b7e194a1f1e69bbc62dfa9a5d126306f08a4e0
Jon Graf <jongraf1@gmail.com>
2020-10-23 15:16:21 -0500

    cleaned up home automation.  I need to enable state

--- src/components/controls/LightSwitch.js
+++ src/components/controls/LightSwitch.js
@@ -1,52 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { debounchApi } from '../../utilities/Services';
 import { CustomSlider } from '../../components/controls/Slider';
 import { ButtonBase } from '@material-ui/core';
 import SwitchSlider from '../segments/SwitchSlider';
-import { setLightGroupState, setLightState } from '../../utilities/RestApi';
+import { setLightGroupState } from '../../utilities/RestApi';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import './LightSwitch.css';
 
 export default function LightSwitch(props) {
-    // const [on, setOn] = useState(props.data.on);
+    const [lights, ] = useState(props.data.lights);
     const [groupId,] = useState(props.data.groupId);
     const [groupName,] = useState(props.data.groupName);
     const [brightness, setBrightness] = useState(Math.round(props.data.brightness / 2.55));
-    const [lights, setLights] = useState(props.data.lights);
     const [areLightsOpen, setLightsOpen] = useState(false);
 
     const sliderToggleLightGroup = async (event, value) => {
         const newBrightness = Math.round(value * 2.55);
-        //probably wont need on anymore
-        // const lightGroupState = !on;
         debounchApi(() => setLightGroupState(groupId, true, newBrightness));
-        lights.forEach(x => { x.brightness = newBrightness });
-        // const test = lights.map(x => {return {...x, brightness: newBrightness}});
-        // console.log(test)
-        // setLights(test);
-        // setOn(lightGroupState);
         setBrightness(value)
     };
 
     const getLightSwitches = () => {
         if (lights && lights.length > 0) {
             return lights.map(x => (
-                <SwitchSlider data={x} parent={brightness}/>
-                // <div className="light-group" key={`light-group-${x.lightId}`}>
-                //     <p className="light-text">{x.lightName}</p>
-                //     <CustomSlider key={`switch-${x.lightId}`} data-testid={"light-switches"} onChange={(ev, val) => {toggleCheckedLight(x, val)}} value={Math.round(x.brightness / 2.55)} valueLabelDisplay="auto" aria-label="slider" />
-                // </div>
+                <SwitchSlider key={`switch-${x.lightId}`}  data={x} parent={brightness} />
             ));
         }
         return <p className="panel-text">No lights assigned to group</p>
-    };
-
-    const toggleCheckedLight = async (light, value) => {
-        const filteredLights = lights.filter(x => x.lightId !== light.lightId);
-        const newLight = { lightId: light.lightId, lightName: light.lightName, on: !light.on, brightness: light.brightness };
-        filteredLights.push(newLight);
-        setLights(filteredLights);
-        debounchApi(() => setLightState(light.lightId, true, Math.round(value * 2.55)));
     };
 
     return (
--- src/components/segments/SwitchSlider.js
+++ src/components/segments/SwitchSlider.js
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { debounchApi } from '../../utilities/Services';
 import { setLightState } from '../../utilities/RestApi';
 import { CustomSlider } from '../controls/Slider';
 
 
 export default function SwitchSlider(props) {
-    const [light, ] = useState(props.data);
-    const [on, setOn] = useState(props.data.on);
-    const [lightId, setLightId] = useState(props.data.lightId);
-    const [lightName, setLightName] = useState(props.data.lightName);
+    const [on, ] = useState(props.data.on);
+    const [lightId, ] = useState(props.data.lightId);
+    const [lightName, ] = useState(props.data.lightName);
     const [brightness, setBrightness] = useState(Math.round(props.data.brightness / 2.55));
-
-    useEffect(() => {
-        console.log(`Parent Brightness: ${props.parent}`)
-        console.log(`Child Brightness: ${brightness }`)
-        // setOn(props.data.on);
-        // setLightId(lightId);
-        // setLightName(lightName);
-        // setBrightness(Math.round(props.data.brightness / 2.55));
-    }, [brightness]);
 
     const toggleCheckedLight = (event, value) => {
         setBrightness(value);
-        // console.log(`Value: ${value}`)
-        // debounchApi(() => setLightState(lightId, true, value * 2.55));
+        debounchApi(() => setLightState(lightId, true, value * 2.55));
     }
 
     return (
-        <div className="light-group" key={`light-group-${lightId}`}>
+        <div className="light-group">
             <p className="light-text">{lightName}</p>
-            <CustomSlider key={`switch-${lightId}`} data-testid={"light-switch"} onChange={(event, val) => toggleCheckedLight(event, val)} value={brightness} valueLabelDisplay="auto" aria-label="slider" />
+            <CustomSlider data-testid={"light-switch"} onChange={(event, val) => toggleCheckedLight(event, val)} value={brightness} valueLabelDisplay="auto" aria-label="slider" />
         </div>
     )
 }
