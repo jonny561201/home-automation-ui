7d97e4d4f2cc1710edbd468fff1dcb2f494afc22
Jon Graf <jongraf1@gmail.com>
2020-11-06 16:14:09 -0600

    added test so that the time picker defaults the value when not supplied an initial time

--- src/tests/components/controls/TimePicker.spec.js
+++ src/tests/components/controls/TimePicker.spec.js
@@ -6,13 +6,20 @@ import TimePicker from '../../../components/controls/TimePicker';
 describe('Time Picker', () => {
     const time = '09:00:00';
 
-    const renderComponent = async () => {
-        render(<TimePicker initialTime={time}/>);
+    const renderComponent = async (setTime) => {
+        render(<TimePicker initialTime={setTime}/>);
     }
 
-    it('should display the current time', async () => {
-        await renderComponent();
+    it('should display the provided time', async () => {
+        await renderComponent(time);
         const actual = screen.getByRole('textbox');
         expect(actual.value).toEqual('09:00 AM')
+    });
+
+    it('should display the current time when initial value not supplied', async () => {
+        await renderComponent(null);
+        const actual = screen.getByRole('textbox');
+        const date = new Date();
+        expect(actual.value).toContain(`${date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`)
     });
 });
