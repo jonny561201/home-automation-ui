2d7a62b01e225041bdb2d01d163a60c0a680286f
Jon Graf <jongraf1@gmail.com>
2020-11-06 14:44:49 -0600

    added test for the day of the week picker to ensure it displays multiple day pickers

--- src/tests/components/controls/WeekPicker.spec.js
+++ src/tests/components/controls/WeekPicker.spec.js
@@ -5,7 +5,7 @@ import WeekPicker from '../../../components/controls/WeekPicker';
 
 
 describe('Week Picker', () => {
-    const daysOfWeek = [{id: 'Mon', on: false, day: 'M'}];
+    let daysOfWeek;
 
     const renderComponent = async () => {
         render(
@@ -15,9 +15,23 @@ describe('Week Picker', () => {
         );
     }
 
+    beforeEach(() => {
+        daysOfWeek = [{id: 'Mon', on: false, day: 'M'}]
+    });
+
     it('should display a daypicker component', async () => {
         await renderComponent();
         const actual = screen.getByText('M').textContent;
         expect(actual).toEqual('M');
+    });
+
+    it('should display multiple days of the week', async () => {
+        daysOfWeek.push({id: 'Tue', on: false, day: 'T'});
+        await renderComponent();
+        const actualMon = screen.getByText('M').textContent;
+        const actualTues = screen.getByText('T').textContent;
+
+        expect(actualMon).toEqual('M');
+        expect(actualTues).toEqual('T');
     });
 });
