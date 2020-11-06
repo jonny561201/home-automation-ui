e4a7704c7a41b9f1a4c560e4c078c8805dfbbe67
Jon Graf <jongraf1@gmail.com>
2020-11-06 13:30:07 -0600

    updated settings panel to toggle on the selected buttons in global state

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { Context } from '../state/Store';
 import Header from '../components/header/Header';
 import { getStore } from '../state/GlobalState';
 import { getUserPreferences } from '../utilities/RestApi';
@@ -8,12 +9,14 @@ import './Settings.css'
 
 
 export default function Settings() {
+    const [state, dispatch] = useContext(Context);
     getStore().setActivePage('Settings');
     const [userId,] = useState(getStore().getUserId());
     const [city, setCity] = useState();
     const [tempUnit, setTempUnit] = useState();
     const [isEditMode, setEditMode] = useState();
     const [lightDays, setLightDays] = useState();
+    const [lightGroupId, setLightGroupId] = useState();
     const [lightTime, setLightTime] = useState();
     const [groupName, setGroupName] = useState();
     const [measureUnit, setMeasureUnit] = useState();
@@ -25,8 +28,13 @@ export default function Settings() {
             setTempUnit(response.temp_unit);
             setMeasureUnit(response.measure_unit);
             setLightDays(response.light_alarm.alarm_days);
+            setLightGroupId(response.light_alarm.alarm_light_group)
             setLightTime(response.light_alarm.alarm_time);
             setGroupName(response.light_alarm.alarm_group_name);
+
+            state.daysOfWeek.filter(x => response.light_alarm.alarm_days.includes(x.id)).map(y =>
+                dispatch({type: 'TOGGLE_DAY_OF_WEEK', payload: {...y, on: true} })
+            )
         };
         getData();
     }, [userId]);
@@ -47,9 +55,9 @@ export default function Settings() {
                         tempUnit={tempUnit} setTempUnit={setTempUnit}
                         isEditMode={isEditMode} setEditMode={setEditMode}
                         measureUnit={measureUnit} setMeasureUnit={setMeasureUnit}
-                        days={lightDays}
                         time={lightTime}
-                        groupName={groupName}/>
+                        groupName={groupName}
+                        groupId={lightGroupId}/>
                     : <SettingsPanel
                         city={city}
                         tempUnit={tempUnit}
