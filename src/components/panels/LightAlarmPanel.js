e65a2559bcb2686747aef0d9ff32d15df3460e4f
Jon Graf <jongraf1@gmail.com>
2021-01-18 17:48:20 -0600

    updated activities to be stored in an expansion panel

--- src/components/panels/LightAlarmPanel.js
+++ src/components/panels/LightAlarmPanel.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Divider } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { ExpansionPanelDetails, ExpansionPanel, Typography, ExpansionPanelSummary } from '@material-ui/core';
 
 
 export default function LightAlarm(props) {
 
     return (
         <>
-            <div className="alarm-setting-group" data-testid="light-alarm-group">
-                <div className="settings-row alarm-row">
-                    <p className="settings-text alarm-time">{props.lightTime}</p>
-                </div>
-                <div className="settings-row alarm-row">
-                    <p className="settings-text alarm-group-name">{props.groupName}</p>
-                </div>
-            </div>
-            <div className="settings-row alarm-row">
-                <p className="settings-text measure-unit">{props.lightDays}</p>
-            </div>
-            <Divider className="alarm-time-divider"/>
+            <ExpansionPanel className="task-panel">
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <div>
+                        <div className="alarm-setting-group" data-testid="light-alarm-group">
+                            <div className="settings-row alarm-row">
+                                <p className="settings-text alarm-time">{props.lightTime}</p>
+                            </div>
+                            <div className="settings-row alarm-row">
+                                <p className="settings-text alarm-group-name">{props.groupName}</p>
+                            </div>
+                        </div>
+                        <div className="settings-row alarm-row">
+                            <p className="settings-text measure-unit">{props.lightDays}</p>
+                        </div>
+                    </div>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails className="center">
+                    <Typography>Test Detail line 2</Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
         </>
     )
 }
