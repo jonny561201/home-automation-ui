3a68f817dfb3d5fa1654863baddf627c0713918a
Jon Graf <jongraf1@gmail.com>
2021-01-18 15:05:29 -0600

    aligned light alarm rows and cleaned up unused row

--- src/components/panels/LightAlarmPanel.js
+++ src/components/panels/LightAlarmPanel.js
@@ -8,16 +8,13 @@ export default function LightAlarm(props) {
         <>
             <div className="alarm-setting-group">
                 <div className="settings-row alarm-row">
-                    {/* <p className="settings-text measure-unit">Alarm Time:</p> */}
                     <p className="settings-text alarm-time">{props.lightTime}</p>
                 </div>
                 <div className="settings-row alarm-row">
-                    {/* <p className="settings-text measure-unit">Alarm Room:</p> */}
                     <p className="settings-text alarm-group-name">{props.groupName}</p>
                 </div>
             </div>
-            <div className="settings-row">
-                {/* <p className="settings-text measure-unit">Alarm Days:</p> */}
+            <div className="settings-row alarm-row">
                 <p className="settings-text measure-unit">{props.lightDays}</p>
             </div>
             <Divider className="alarm-time-divider"/>
