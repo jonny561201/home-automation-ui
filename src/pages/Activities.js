892954353c7c168cb1b3ee7a96d1e70b13fc472b
Jon Graf <jongraf1@gmail.com>
2021-01-18 16:23:53 -0600

    created new pages for activities

---
+++ src/pages/Activities.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { Divider } from '@material-ui/core';
+import { getStore } from '../state/GlobalState';
+import LightAlarm from '../components/panels/LightAlarmPanel';
+import { getScheduledTasks } from '../utilities/RestApi';
+import './Activities.css';
+
+
+export default function ActivitiesPage() {
+    const [tasks, setTasks] = useState([]);
+
+
+    useEffect(() => {
+        const getData = async () => {
+            const responseTasks = await getScheduledTasks(getStore().getUserId());
+            setTasks(responseTasks);
+        };
+        getData();
+    }, []);
+
+    return (
+        <>
+            <div className="settings-group settings-text">
+                <h2>Light Alarm</h2>
+            </div>
+            <Divider />
+            {
+                tasks.map(x => {
+                    return <LightAlarm groupName={x.alarm_group_name} lightDays={x.alarm_days} lightTime={x.alarm_time} />
+                })
+            }
+        </>
+    )
+}
---
+++ src/tests/pages/Activities.spec.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import * as lib from '../../utilities/RestApi';
+import { getStore } from '../../state/GlobalState';
+import { render, screen, act } from '@testing-library/react';
+import ActivitiesPage from '../../pages/Activities';
+
+
+describe('Activities Page', () => {
+    const roles = [{"role_name": "lighting"}];
+    const userId = 'ascv123';
+    const room = 'BedRoom';
+    const time = '07:30:00';
+    const days = 'MonTueWedThuFri';
+
+    const spyGet = jest.spyOn(lib, 'getScheduledTasks');
+
+    const renderComponent = async () => {
+        await act(async () => {
+            render(
+                <ActivitiesPage />
+            );
+        });
+    }
+
+    beforeEach(() => {
+        spyGet.mockClear();
+        getStore().setUserId(userId);
+        spyGet.mockReturnValue([{ alarm_group_name: room, alarm_light_group: '2', alarm_days: days, alarm_time: time }]);
+    });
+
+    it('should make api call to get settings data', async () => {
+        await renderComponent();
+        expect(spyGet).toHaveBeenCalledWith(userId)
+    });
+
+    it('should display the light alarm component', async () => {
+        await renderComponent();
+        const actual = screen.getByText('Light Alarm').textContent;
+        expect(actual).toEqual('Light Alarm');
+    });
+
+    it('should not display the light alarm component when zero alarms', async () => {
+        spyGet.mockReturnValue([]);
+        await renderComponent();
+        const actual = screen.queryByTestId('light-alarm-group');
+        expect(actual).toBeNull();
+    });
+});
