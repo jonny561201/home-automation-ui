8ea159d72daf5c9d0c5b704758bf315c986bd19e
Jon Graf <jongraf1@gmail.com>
2021-01-18 17:37:40 -0600

    updated the activities to have a unique key for each light alarm generated

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -33,7 +33,7 @@ export default function ActivitiesPage() {
                     <Divider />
                     {
                         tasks.map(x => {
-                            return <LightAlarm groupName={x.alarm_group_name} lightDays={x.alarm_days} lightTime={x.alarm_time} />
+                            return <LightAlarm key={`${x.alarm_group_name}-${x.alarm_days}-${x.alarm_time}`} groupName={x.alarm_group_name} lightDays={x.alarm_days} lightTime={x.alarm_time} />
                         })
                     }
                 </div>
