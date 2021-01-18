de8677ddc07810c62340a9fc4a57b443f968f820
Jon Graf <jongraf1@gmail.com>
2021-01-18 15:00:43 -0600

    updated light alarm tasks to show with time first, days below, and room name to the right

--- src/components/panels/LightAlarmPanel.js
+++ src/components/panels/LightAlarmPanel.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Divider } from '@material-ui/core';
+
 
 export default function LightAlarm(props) {
 
     return (
         <>
-            <div className="settings-row">
-                <p className="settings-text measure-unit">Alarm Room:</p>
-                <p className="settings-text measure-unit">{props.groupName}</p>
+            <div className="alarm-setting-group">
+                <div className="settings-row alarm-row">
+                    {/* <p className="settings-text measure-unit">Alarm Time:</p> */}
+                    <p className="settings-text alarm-time">{props.lightTime}</p>
+                </div>
+                <div className="settings-row alarm-row">
+                    {/* <p className="settings-text measure-unit">Alarm Room:</p> */}
+                    <p className="settings-text alarm-group-name">{props.groupName}</p>
+                </div>
             </div>
             <div className="settings-row">
-                <p className="settings-text measure-unit">Alarm Days:</p>
+                {/* <p className="settings-text measure-unit">Alarm Days:</p> */}
                 <p className="settings-text measure-unit">{props.lightDays}</p>
             </div>
-            <div className="settings-row">
-                <p className="settings-text measure-unit">Alarm Time:</p>
-                <p className="settings-text measure-unit">{props.lightTime}</p>
-            </div>
+            <Divider className="alarm-time-divider"/>
         </>
     )
 }
--- src/tests/components/panels/LightAlarmPanel.spec.js
+++ src/tests/components/panels/LightAlarmPanel.spec.js
@@ -16,34 +16,16 @@ describe('Light Alarm Panel', () => {
         });
     }
 
-    it('should display the alarm room text for measurement', async () => {
-        await renderComponent();
-        const actual = screen.getByText('Alarm Room:').textContent;
-        expect(actual).toEqual('Alarm Room:');
-    });
-
     it('should display the current alarm room setting stored in state', async () => {
         await renderComponent();
         const actual = screen.getByText(groupName).textContent;
         expect(actual).toEqual(groupName);
     });
 
-    it('should display the alarm time for measurement', async () => {
-        await renderComponent();
-        const actual = screen.getByText('Alarm Time:').textContent;
-        expect(actual).toEqual('Alarm Time:');
-    });
-
     it('should display the current alarm time setting stored in state', async () => {
         await renderComponent();
         const actual = screen.getByText(alarmTime).textContent;
         expect(actual).toEqual(alarmTime);
-    });
-
-    it('should display the alarm days for measurement', async () => {
-        await renderComponent();
-        const actual = screen.getByText('Alarm Days:').textContent;
-        expect(actual).toEqual('Alarm Days:');
     });
 
     it('should display the current alarm days setting stored in state', async () => {
