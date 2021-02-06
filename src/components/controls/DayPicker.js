02644f2b173961fc2f6d2ec3e953a0b477b38d0e
Jon Graf <jongraf1@gmail.com>
2021-02-06 12:37:21 -0600

    added pop sound when selecting days on the day picker

--- src/components/controls/DayPicker.js
+++ src/components/controls/DayPicker.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import useSound from 'use-sound';
+import PopSound from '../../resources/pop.mp3';
 import './DayPicker.css';
 
 
 export default function DayPicker(props) {
+    const [pop] = useSound(PopSound);
     const [on, setOn] = useState(props.day.on);
 
     const toggleDay = () => {
@@ -10,6 +13,7 @@ export default function DayPicker(props) {
         props.toggleDay(props.day, updatedState);
         setOn(updatedState);
         props.setEdited();
+        pop();
     }
 
     return (
