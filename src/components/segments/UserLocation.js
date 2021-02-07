ece5d103e99d55498d1ef076ba28ea3f1918530c
Jon Graf <jongraf1@gmail.com>
2021-02-06 19:06:05 -0600

    going back to a stateful check instead of a nested if check.  Seems easier to avoid certain timing issues associated with falling all the way through an if block

--- src/components/segments/UserLocation.js
+++ src/components/segments/UserLocation.js
@@ -13,6 +13,8 @@ export default function UserLocation() {
     const [state, dispatch] = useContext(Context);
     const [cancel, setCancel] = useState(false);
     const [opened, setOpened] = useState(false);
+    const [firstCheck, setFirstCheck] = useState(false);
+    const [secondCheck, setSecondCheck] = useState(false);
     const [displayMenu, setDisplayMenu] = useState(false);
 
     useEffect(() => {
@@ -31,22 +33,24 @@ export default function UserLocation() {
 
     const shouldOpenGarage = (distance) => {
         // DISTANCE COMES IN AS MILES!!!
-        if (distance >= 0.04 && distance < 0.35) {
-            if (distance <= 0.3 && distance > 0.15) {
-                if (distance <= 0.15 && distance > 0.05) {
-                    setDisplayMenu(true);
-                    if (distance <= 0.05 && !cancel && !opened) {
-                        ding();
-                        setOpened(true);
-                        setDisplayMenu(false);
-                        return true;
-                    }
-                    return false;
-                }
+        // not in garage
+        if (distance >= 0.02 && distance < 0.35) {
+            if (distance > 0.1 && distance <= 0.3 && !firstCheck && !secondCheck) {
+                setFirstCheck(true);
                 return false;
+            } else if (distance > 0.06 && distance <= 0.2 && firstCheck && !secondCheck) {
+                setSecondCheck(true);
+                setDisplayMenu(true);
+                return false;
+            } else if (distance <= 0.04 && secondCheck && firstCheck && !cancel && !opened) {
+                ding();
+                setDisplayMenu(false);
+                setOpened(true);
+                return true;
             }
-            return false;
         } else {
+            setFirstCheck(false);
+            setSecondCheck(false);
             setDisplayMenu(false);
             setCancel(false);
             setOpened(false)
