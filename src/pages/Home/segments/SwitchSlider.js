015c15f4e831096611b113de19d2a455a7cb1d45
Jon Graf <jgraf@leantechniques.com>
2021-04-10 23:42:25 -0500

    added gap to better align individual light sliders

--- src/pages/Home/segments/SwitchSlider.js
+++ src/pages/Home/segments/SwitchSlider.js
@@ -7,7 +7,7 @@ import { CustomSlider } from '../../../components/controls/Slider';
 
 export default function SwitchSlider(props) {
     const [state, dispatch] = useContext(Context);
-    const [lightId, ] = useState(props.lightId);
+    const [lightId,] = useState(props.lightId);
     const [light, setLight] = useState(props.data);
 
     useEffect(() => {
@@ -18,13 +18,14 @@ export default function SwitchSlider(props) {
         const newLight = { ...light, brightness: value * 2.55 };
         setLight(newLight);
         debounchApi(() => setLightState(lightId, true, value * 2.55));
-        dispatch({type: 'SET_USER_LIGHT', payload: newLight});
+        dispatch({ type: 'SET_USER_LIGHT', payload: newLight });
     }
 
     return (
         <div className="light-group">
             <p className="light-text text">{light.lightName}</p>
             <CustomSlider data-testid={"light-switch"} onChange={(event, val) => toggleCheckedLight(event, val)} value={Math.round(light.brightness / 2.55)} valueLabelDisplay="auto" aria-label="slider" />
+            <div style={{ height: '16px', width: '16px' }} />
         </div>
     )
 }
