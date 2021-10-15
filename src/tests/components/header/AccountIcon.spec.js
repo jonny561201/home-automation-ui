3bf7668cfcda5e3b02e096822d03cdd21fe5be92
Jon Graf <jongraf1@gmail.com>
2021-10-15 15:59:14 -0500

    fixed RTL issue with account icon tests not wrapping render in act

--- src/tests/components/header/AccountIcon.spec.js
+++ src/tests/components/header/AccountIcon.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import Account from '../../../components/header/AccountIcon';
 import { getStore } from '../../../state/GlobalState';
 
@@ -7,35 +7,43 @@ describe('Account', () => {
     const firstName = "Jon";
     const lastName = "Rox";
 
+    const renderComponent = async () => {
+        await act(async () => {
+            render(
+                <Account />
+            );
+        });
+    }
+
     beforeEach(() => {
         getStore().setFirstName(firstName);
         getStore().setLastName(lastName);
     });
 
-    it('should display account login border', () => {
-        render(<Account />);
+    it('should display account login border', async () => {
+        await renderComponent();
         const actual = screen.getByTestId('account-border');
         expect(actual).toBeDefined();
     });
 
-    it('should display account login token', () => {
-        render(<Account />);
+    it('should display account login token', async () => {
+        await renderComponent();
         const actual = screen.getByTestId('account-center');
         expect(actual).toBeDefined();
     });
 
-    it('should display the first letter of users first name and first letter of users last name', () => {
-        render(<Account />);
+    it('should display the first letter of users first name and first letter of users last name', async () => {
+        await renderComponent();
         const actual = screen.getByText('JR').textContent;
         expect(actual).toEqual("JR");
     });
 
-    it('should trim whitespace from users first and last name', () => {
+    it('should trim whitespace from users first and last name', async () => {
         const firstName = " Jon";
         const lastName = " Rox";
         getStore().setFirstName(firstName);
         getStore().setLastName(lastName);
-        render(<Account />);
+        await renderComponent();
 
         const actual = screen.getByText('JR').textContent;
         expect(actual).toEqual("JR");
