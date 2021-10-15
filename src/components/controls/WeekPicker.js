35cb858d47f233ff9b925327fd4697502b81dad7
Jon Graf <jongraf1@gmail.com>
2021-10-15 15:44:13 -0500

    undoing unnecessary use state method use in week picker

--- src/components/controls/WeekPicker.js
+++ src/components/controls/WeekPicker.js
@@ -1,18 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import DayPicker from './DayPicker';
 import './WeekPicker.css'
 
 
 export default function WeekPicker(props) {
-    const [days, setDays] = useState([]);
-
-    useEffect(() => {
-        setDays(props.daysOfWeek)
-    })
 
     return (
         <div className="weekday-picker">
-            {days.map((weekday) => (
+            {props.daysOfWeek.map((weekday) => (
                 <DayPicker key={weekday.id + "-day-picker"} day={weekday} toggleDay={props.toggleDay} setEdited={props.setEdited} />
             ))}
         </div>
