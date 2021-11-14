d967803ec807967c71a0f1fa782c5cc80aaf4bef
Jon Graf <jongraf1@gmail.com>
2021-11-13 18:54:52 -0600

    fixed some minor light switch styles

--- src/components/controls/LightSwitch.js
+++ src/components/controls/LightSwitch.js
@@ -53,7 +53,7 @@ export default function LightSwitch(props) {
                     </div>
                 </ButtonBase>
                 <div className="light-ripple">
-                <p className="light-text text" onClick={toggleLightGroup}>{groupName}</p>
+                    <p className="light-text text" onClick={toggleLightGroup}>{groupName}</p>
                 </div>
                 <CustomSlider data-testid={"light-group-switch"} onChange={sliderToggleLightGroup} value={brightness} valueLabelDisplay="auto" aria-label="slider" />
                 <BrightnessMediumIcon className="brightness-icon text" />
