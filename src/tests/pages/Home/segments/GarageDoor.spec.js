b0779dfe57c95f565d4a44a279cac77152fca7c5
Jon Graf <jongraf1@gmail.com>
2022-02-09 21:52:51 -0600

    updated additional styles for the garage door component text

--- src/tests/pages/Home/segments/GarageDoor.spec.js
+++ src/tests/pages/Home/segments/GarageDoor.spec.js
@@ -32,44 +32,26 @@ describe('Garage Door', () => {
 
     it('should show close garage button', async () => {
         await renderComponent(device);
-        const actual = screen.getByTestId('update-garage-close').textContent;
+        const actual = screen.getByText('Close').textContent;
         expect(actual).toEqual('Close');
     });
 
     it('should show open garage button', async () => {
         await renderComponent({ ...device, isOpen: false });
-        const actual = screen.getByTestId('update-garage-open').textContent;
+        const actual = screen.getByText('Open').textContent;
         expect(actual).toEqual('Open');
     });
 
     it('should show toggle garage button', async () => {
         await renderComponent(device);
-        const actual = screen.getByTestId('toggle-garage-button').textContent;
+        const actual = screen.getByText('Toggle').textContent;
         expect(actual).toEqual('Toggle');
-    });
-
-    it('should show Door text', async () => {
-        await renderComponent(device);
-        const actual = screen.getByText('Door:').textContent;
-        expect(actual).toEqual('Door: ');
     });
 
     it('should show Door Name text', async () => {
         await renderComponent(device);
         const actual = screen.getByText(doorName).textContent;
         expect(actual).toEqual(doorName);
-    });
-
-    it('should show Door Status text', async () => {
-        await renderComponent(device);
-        const actual = screen.getByText('Status:').textContent;
-        expect(actual).toEqual('Status: ');
-    });
-
-    it('should show Duration text', async () => {
-        await renderComponent(device);
-        const actual = screen.getByText('Duration:').textContent;
-        expect(actual).toEqual('Duration: ');
     });
 
     it('should display Close text when response is false', async () => {
@@ -80,8 +62,8 @@ describe('Garage Door', () => {
 
     it('should display Open text when response is true', async () => {
         await renderComponent(device);
-        const actual = screen.getByText("Open").textContent;
-        expect(actual).toEqual('Open');
+        const actual = screen.getByText("Opened").textContent;
+        expect(actual).toEqual('Opened');
     });
 
     describe('garage door api', () => {
@@ -89,20 +71,20 @@ describe('Garage Door', () => {
         it('should call update function with false when closing', async () => {
             spyUpdate.mockReturnValue({ isOpen: false });
             await renderComponent({ ...device, isOpen: true });
-            userEvent.click(screen.getByTestId("update-garage-close"));
+            userEvent.click(screen.getByText("Close"));
             expect(spyUpdate).toBeCalledWith(userId, bearer, false, 1);
         });
 
         it('should call update function with true when opening', async () => {
             spyUpdate.mockReturnValue({ isOpen: true });
             await renderComponent({ ...device, isOpen: false });
-            userEvent.click(screen.getByTestId("update-garage-open"));
+            userEvent.click(screen.getByText("Open"));
             expect(spyUpdate).toBeCalledWith(userId, bearer, true, 1);
         });
 
         it('should call toggle function', async () => {
             await renderComponent(device);
-            userEvent.click(screen.getByTestId("toggle-garage-button"));
+            userEvent.click(screen.getByText('Toggle'));
 
             expect(spyToggle).toBeCalledWith(userId, bearer, 1);
         });
