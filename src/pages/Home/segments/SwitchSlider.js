c2dd75a97bcd3b473ed45ab79ed92572a6774171
Jon Graf <jongraf1@gmail.com>
2022-02-16 17:19:27 -0600

    updated the individual light switch sliders to toggle light on and off

--- src/pages/Home/segments/SwitchSlider.js
+++ src/pages/Home/segments/SwitchSlider.js
@@ -11,6 +11,7 @@ export default function SwitchSlider(props) {
     const [light, setLight] = useState(props.data);
     const [lightId,] = useState(props.data.lightId);
     const [groupId,] = useState(props.data.groupId);
+    const [prevBrightness, setPrevBrightness] = useState(0);
 
     useEffect(() => {
         const group = state.lights.find(x => x.groupId === props.data.groupId)
@@ -19,17 +20,29 @@ export default function SwitchSlider(props) {
     });
 
     const updateSlider = (event, value) => {
-        const newLight = { ...light, brightness: value * 2.55 };
+        const newLight = { ...light, brightness: value * 2.55, on: value > 0 };
         setLight(newLight);
         debounchApi(() => setLightState(state.auth.bearer, lightId, true, value * 2.55));
         const newList = state.lights.map(x => (x.groupId === groupId) ? { ...x, lights: x.lights.map(y => (y.lightId === lightId) ? newLight : y) } : x);
         dispatch({ type: 'SET_LIGHTS', payload: newList });
     }
 
+    const toggleLight = async () => {
+        const newState = !light.on;
+        const newBrightness = !newState ? 0 : prevBrightness;
+        await setLightState(state.auth.bearer, lightId, true, newBrightness)
+        setPrevBrightness(light.brightness);
+
+        const newLight = { ...light, brightness: newBrightness, on: newState };
+        const newList = state.lights.map(x => (x.groupId === groupId) ? { ...x, lights: x.lights.map(y => (y.lightId === lightId) ? newLight : y) } : x);
+        setLight(newLight);
+        dispatch({ type: 'SET_LIGHTS', payload: newList });
+    }
+
     return (
         <div className="light-group">
             <div className="light-spacer-one"/>
-            <ButtonBase className="light-button">
+            <ButtonBase className="light-button" onClick={toggleLight}>
                 <p className="light-text-small text">{light.lightName}</p>
             </ButtonBase>
             <CustomSlider onChange={(event, val) => updateSlider(event, val)} value={Math.round(light.brightness / 2.55)} valueLabelDisplay="auto" aria-label="slider" />
