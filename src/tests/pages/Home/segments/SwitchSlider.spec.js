b46e730240c2f056dadd44ef1cac213c4b972df4
Jon Graf <jongraf1@gmail.com>
2022-02-16 18:00:23 -0600

    added test to make sure individual light sliders turn light back on as well

--- src/tests/pages/Home/segments/SwitchSlider.spec.js
+++ src/tests/pages/Home/segments/SwitchSlider.spec.js
@@ -15,7 +15,7 @@ describe('SwitchSlider', () => {
     }
     const spySetLight = jest.spyOn(lib, 'setLightState');
 
-    const renderComponent = async () => {
+    const renderComponent = async (group) => {
         await act(async () => {
             render(
                 <Context.Provider value={[{ lights: [group], auth: { bearer: bearer } }, () => { }]}>
@@ -30,7 +30,7 @@ describe('SwitchSlider', () => {
     });
 
     // it('should make api call when toggling on the desk lamp', async () => {
-    //     await renderComponent();
+    //     await renderComponent(group);
 
     //     await act(async () => {
     //         // fireEvent.change(screen.getByTestId('light-switch'), { target: { value: 25 } });
@@ -41,21 +41,35 @@ describe('SwitchSlider', () => {
     // });
 
     it('should display the light name', async () => {
-        await renderComponent();
+        await renderComponent(group);
         expect((screen.getByText('desk lamp')).textContent).toEqual('desk lamp');
     });
 
     it('should display the slider', async () => {
-        await renderComponent();
+        await renderComponent(group);
         expect((screen.getByRole('slider'))).toBeDefined();
     });
 
     it('should make api call to turn off light on first click of button', async () => {
-        await renderComponent();
+        await renderComponent(group);
         await act(async () => {
             fireEvent.click(screen.getByRole('button', {name: light.lightName}));
         });
 
         expect(spySetLight).toHaveBeenCalledWith(bearer, light.lightId, false, 0);
+    });
+
+    it('should make api call to turn on light on first click of button', async () => {
+        const light = { 'lightName': 'desk lamp', 'on': false, 'brightness': 123, 'lightId': lightId, 'groupId': 1 };
+        const offGroup = {
+            'groupId': 1, 'groupName': 'Living Room', 'on': true,
+            'brightness': 155, 'lights': [light]
+        }
+        await renderComponent(offGroup);
+        await act(async () => {
+            fireEvent.click(screen.getByRole('button', {name: light.lightName}));
+        });
+
+        expect(spySetLight).toHaveBeenCalledWith(bearer, light.lightId, true, 0);
     });
 });
