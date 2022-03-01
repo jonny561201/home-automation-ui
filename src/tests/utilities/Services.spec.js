10494533997a79665c7fcf748fb98e300013bf81
Jon Graf <jongraf1@gmail.com>
2022-02-28 21:55:38 -0600

    created a new function to determine if it is daylight as it should be more straightforward then night time

--- src/tests/utilities/Services.spec.js
+++ src/tests/utilities/Services.spec.js
@@ -1,4 +1,4 @@
-import { parseDate } from '../../utilities/Services';
+import {  isDayLight, parseDate } from '../../utilities/Services';
 
 
 describe('Services', () => {
@@ -17,5 +17,157 @@ describe('Services', () => {
         const actual = parseDate(time);
 
         expect(actual.getMinutes()).toEqual(23);
+    });
+
+    describe('isDayLight', () => {
+        const lat = 41.621208;
+        const long = -93.831599;
+        const coords = {latitude: lat, longitude: long};
+
+        describe('Garage Door Coords', () => {
+
+            it('should return true when One PM', () => {
+                const febTwentyEight = new Date(1646074800000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return true when Seven AM', () => {
+                const febTwentyEight = new Date(1646053200000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return true when Twelve PM', () => {
+                const febTwentyEight = new Date(1646071200000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return true when Five PM', () => {
+                const febTwentyEight = new Date(1646089200000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return false when One AM', () => {
+                const febTwentyEight = new Date(1646031600000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Eight PM', () => {
+                const febTwentyEight = new Date(1646100000000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Seven PM', () => {
+                const febTwentyEight = new Date(1646096400000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Six AM', () => {
+                const febTwentyEight = new Date(1646096400000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Eleven PM', () => {
+                const febTwentyEight = new Date(1646110800000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Twelve AM', () => {
+                const febTwentyEight = new Date(1646028000000);
+                const actual = isDayLight(coords, null, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+        });
+
+        describe('User Coords', () => {
+
+            it('should return true when One PM', () => {
+                const febTwentyEight = new Date(1646074800000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return true when Seven AM', () => {
+                const febTwentyEight = new Date(1646053200000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return true when Twelve PM', () => {
+                const febTwentyEight = new Date(1646071200000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return true when Five PM', () => {
+                const febTwentyEight = new Date(1646089200000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeTruthy()
+            });
+    
+            it('should return false when One AM', () => {
+                const febTwentyEight = new Date(1646031600000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Eight PM', () => {
+                const febTwentyEight = new Date(1646100000000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Seven PM', () => {
+                const febTwentyEight = new Date(1646096400000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Six AM', () => {
+                const febTwentyEight = new Date(1646096400000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Eleven PM', () => {
+                const febTwentyEight = new Date(1646110800000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+    
+            it('should return false when Twelve AM', () => {
+                const febTwentyEight = new Date(1646028000000);
+                const actual = isDayLight(null, coords, febTwentyEight);
+    
+                expect(actual).toBeFalsy()
+            });
+        });
     });
 });
--- src/utilities/Services.js
+++ src/utilities/Services.js
@@ -31,7 +31,6 @@ export const parseDate = (time) => {
     return date;
 }
 
-//TODO: test this function???
 export const isNightTime = (garageCoords, userCoords) => {
     if (garageCoords !== null) {
         const today = new Date();
@@ -43,6 +42,20 @@ export const isNightTime = (garageCoords, userCoords) => {
         const sunrise = calculateSunrise(userCoords.latitude, userCoords.longitude);
         const sunset = calculateSunset(userCoords.latitude, userCoords.longitude);
         return (today >= sunset && today < sunrise);
+    }
+}
+
+export const isDayLight = (garageCoords, userCoords, today = new Date()) => {
+    const newDate = new Date(today);
+    newDate.setDate(newDate.getDate() + 1)
+    if (garageCoords !== null) {
+        const sunrise = getSunrise(garageCoords.latitude, garageCoords.longitude, today);
+        const sunset = getSunset(garageCoords.latitude, garageCoords.longitude, newDate);
+        return (today >= sunrise && today < sunset);
+    } else if (userCoords !== null) {
+        const sunrise = getSunrise(userCoords.latitude, userCoords.longitude, today);
+        const sunset = getSunset(userCoords.latitude, userCoords.longitude, newDate);
+        return (today >= sunrise && today < sunset);
     }
 }
 
