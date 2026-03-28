cd8ac03d0e3623a0f52977e635bfa4e1955059d4
Jon Graf <jgraf@vermeer.com>
2026-03-28 12:48:36 -0500

    finished migrated to vite and vite test and modernizing them to the latest version

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.[jt]sx?$/,
-    exclude: [],
-  },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
