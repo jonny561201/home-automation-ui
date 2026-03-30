567cd94ec97f4bab8c765e93e063d91fa24092b6
Jon Graf <jgraf@vermeer.com>
2026-03-30 10:28:44 -0500

    wrapping app entry point in auth provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <Auth0Provider domain="dev-mx0anv661qiyofk8.us.auth0.com" clientId="HCu5saPrukQyCYzcXYANtVKzjg9gpIU5" authorizationParams={{redirect_uri: window.location.origin}}>
+            <App/>
+        </Auth0Provider>
+    </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
