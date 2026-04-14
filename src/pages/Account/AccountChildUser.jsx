7d58efedc6621ac1270bb45b3c04637d4b20b152
Jon Graf <jgraf@vermeer.com>
2026-04-14 14:19:44 -0500

    added cards for mobile screen and removed the table

--- src/pages/Account/AccountChildUser.jsx
+++ src/pages/Account/AccountChildUser.jsx
@@ -13,7 +13,7 @@ export default function AccountChildUser() {
     const roles = state.user.roles || [];
     const [selectedRole, setSelectedRole] = useState([]);
     const [email, setEmail] = useState("");
-    const [test, setTest] = useState([]);
+    const [childAccounts, setChildAccounts] = useState([]);
     const [isEmailInvalid, setIsEmailInvalid] = useState(undefined);
     const [isRoleInvalid, setIsRoleInvalid] = useState(undefined);
 
@@ -22,7 +22,7 @@ export default function AccountChildUser() {
             const token = await auth0.getAccessTokenSilently();
             const response = await getUserChildAccounts(token);
             if (Array.isArray(response)) {
-                setTest(response);
+                setChildAccounts(response);
             }
         };
         getData();
@@ -33,7 +33,7 @@ export default function AccountChildUser() {
         if ((!isEmailInvalid && !isRoleInvalid) && (selectedRole.length !== 0 && email !== null && email !== "")) {
             const token = await auth0.getAccessTokenSilently();
             const response = await addUserChildAccount(token, email, selectedRole);
-            setTest(response);
+            setChildAccounts(response);
             setEmail("");
             setSelectedRole([]);
         } else {
@@ -46,7 +46,7 @@ export default function AccountChildUser() {
         const token = await auth0.getAccessTokenSilently();
         const response = await deleteUserChildAccount(token, childUserId);
         if (response.ok)
-            setTest(test.filter(x => x.user_id !== childUserId));
+            setChildAccounts(childAccounts.filter(x => x.user_id !== childUserId));
     }
 
     const validateEmail = (input) => {
@@ -61,37 +61,45 @@ export default function AccountChildUser() {
 
     return (
         <div className="child-user-container">
-            <h2 className="panel-header-text">Account Users</h2>
-            <Divider />
-            {test.map(x => (
-                <div className="row align-items-center child-user-row" key={`user-${x.user_name}`}>
-                    <div className="col-sm">{x.user_name}</div>
-                    <div className="col-sm">{x.roles.join(', ')}</div>
-                    <div className="col-sm-auto">
-                        <RemoveButton aria-label={`user-${x.user_name}`} onClick={() => deleteChildUser(x.user_id)}></RemoveButton>
+            <h2 className="panel-header-text">Managed Accounts</h2>
+            <div>
+                <p className="small-text text">Grant access to your devices</p>
+                <Divider />
+                {childAccounts.map(x => (
+                    <div className="row align-items-center child-user-row" key={`user-${x.user_name}`}>
+                        <div className="col-sm">{x.user_name}</div>
+                        <div className="col-sm">{x.roles.join(', ')}</div>
+                        <div className="col-sm-auto">
+                            <RemoveButton aria-label={`user-${x.user_name}`} onClick={() => deleteChildUser(x.user_id)}></RemoveButton>
+                        </div>
                     </div>
-                </div>
-            ))}
-            <div className="row align-items-center child-user-row">
-                <div className="col-sm user-input">
-                    <TextField error={isEmailInvalid} onChange={(input) => validateEmail(input)} value={email} label="Email" fullWidth />
-                </div>
-                <div className="col-sm user-input">
-                    <FormControl error={isRoleInvalid} variant="outlined" fullWidth>
-                        <InputLabel>Roles</InputLabel>
-                        <Select labelId="mutiple-name-label" variant="outlined" multiple value={selectedRole} onChange={(input) => validateRole(input)} input={<OutlinedInput label="Roles" />}
-                            renderValue={(selectedRole) => (selectedRole.join(', '))}>
-                            {roles.map((role) => (
-                                <MenuItem key={role.role_name} value={role.role_name}>
-                                    <Checkbox checked={selectedRole.indexOf(role.role_name) > -1} />
-                                    <ListItemText primary={role.role_name} />
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </div>
-                <div className="col-sm-auto">
-                    <AddButton onClick={() => { submitChildAccount() }}></AddButton>
+                ))}
+            </div>
+
+            <div style={{ marginTop: '2rem' }}>
+                <p className="small-text text">Add a new user</p>
+                <Divider />
+                <div className="row align-items-center child-user-row">
+                    <div className="col-sm user-input">
+                        <TextField error={isEmailInvalid} onChange={(input) => validateEmail(input)} value={email} label="Email" fullWidth />
+                    </div>
+                    <div className="col-sm user-input">
+                        <FormControl error={isRoleInvalid} variant="outlined" fullWidth>
+                            <InputLabel>Roles</InputLabel>
+                            <Select labelId="mutiple-name-label" variant="outlined" multiple value={selectedRole} onChange={(input) => validateRole(input)} input={<OutlinedInput label="Roles" />}
+                                    renderValue={(selectedRole) => (selectedRole.join(', '))}>
+                                {roles.map((role) => (
+                                    <MenuItem key={role.role_name} value={role.role_name}>
+                                        <Checkbox checked={selectedRole.indexOf(role.role_name) > -1} />
+                                        <ListItemText primary={role.role_name} />
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className="col-sm-auto user-input">
+                        <AddButton onClick={() => { submitChildAccount() }}></AddButton>
+                    </div>
                 </div>
             </div>
         </div>
